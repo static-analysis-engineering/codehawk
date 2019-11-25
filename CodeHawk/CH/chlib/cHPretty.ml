51efed2a413a3b5ab1fbe038b874495f94b2b429
Henny Sipma <hennysipma@mac.com>
2019-11-25 13:49:24 -0800

    add opaque compiler switch

--- CodeHawk/CH/chlib/cHPretty.ml
+++ CodeHawk/CH/chlib/cHPretty.ml
@@ -23,7 +23,7 @@
    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
    SOFTWARE.
-  ------------------------------------------------------------------------------ *)
+   ============================================================================= *)
 
 type pretty_t =
   | STR of string
