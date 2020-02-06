aa6ce1a7c3537ed29c151cb2573e8bf1100c9b7a
Henny Sipma <hennysipma@mac.com>
2020-02-06 13:02:35 -0800

    CHC:function summaries

--- CodeHawk/CHC/cchsummaries/makejar.sh
+++ CodeHawk/CHC/cchsummaries/makejar.sh
@@ -2,5 +2,16 @@
 
 jar -cf cchsummaries.jar \
     assert \
+    builtins \
+    ctype \
+    dirent \
+    errno \
+    fcntl \
+    mman \
+    signal \
+    stat \
+    stdio  \
     stdlib \
     string \
+    time \
+    unistd \
