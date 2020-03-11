ed6caf1476c00a01451665e815156131ccd11ce6
Henny Sipma <hennysipma@mac.com>
2020-03-11 12:01:19 -0700

    CHB:script file to create summaries jar file

---
+++ CodeHawk/CHB/bchsummaries/makejar.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# gdi : graphics device interface
+
+jar -cf bchsummaries.jar \
+  typedefinitions.xml \
+  structs \
+  constants \
+  jni \
+  rtl_lib \
+  advapi32_dll \
+  api-ms-win-crt-convert-l1-1-0_dll \
+  api-ms-win-crt-heap-l1-1-0_dll \
+  api-ms-win-crt-runtime-l1-1-0_dll \
+  api-ms-win-crt-stdio-l1-1-0_dll \
+  api-ms-win-crt-string-l1-1-0_dll \
+  api-ms-win-crt-utility-l1-1-0_dll \
+  avicap32_dll \
+  comctl32_dll \
+  comdlg32_dll \
+  crtdll_dll \
+  crypt32_dll \
+  dwmapi_dll \
+  gdi32_dll \
+  imm32_dll \
+  iphlpapi_dll \
+  kernel32_dll \
+  lz32_dll \
+  mscoree_dll \
+  msvcrt_dll \
+  msvfw32_dll \
+  mswsock_dll \
+  netapi32_dll \
+  ntdll_dll \
+  odbc32_dll \
+  ole32_dll \
+  oleaut32_dll \
+  oledlg_dll \
+  opengl32_dll \
+  pe-vs-static \
+  psapi_dll \
+  rpcrt4_dll \
+  secur32_dll \
+  shell32_dll \
+  shlwapi_dll \
+  so_functions \
+  spoolss_dll \
+  user32_dll \
+  version_dll \
+  urlmon_dll \
+  userenv_dll \
+  winhttp_dll \
+  wininet_dll \
+  winmm_dll \
+  winspool_drv \
+  winspool_dll \
+  wsock32_dll \
+  ws2_32_dll \
