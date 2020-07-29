ef9215228e513ad67a2ff5539ef39b297ff3f0be
Henny Sipma <hennysipma@mac.com>
2020-07-28 18:55:44 -0700

    CHB:support call targets with function summaries

--- CodeHawk/CHB/bchlib/bCHMakeCallTargetInfo.ml
+++ CodeHawk/CHB/bchlib/bCHMakeCallTargetInfo.ml
@@ -32,7 +32,9 @@ open CHXmlDocument
 open BCHCallTargetInfo
 open BCHDoubleword
 open BCHFunctionApi
+open BCHFunctionInfo
 open BCHFunctionSemantics
+open BCHFunctionSummaryLibrary
 open BCHLibTypes
 
 
@@ -40,22 +42,42 @@ let mk_default_target (name:string) (tgt:call_target_t) =
   let api = default_function_api name [] in
   mk_call_target_info api default_function_semantics tgt
 
+let mk_function_summary_target (fs:function_summary_int) (tgt:call_target_t) =
+  let api = fs#get_function_api in
+  let sem = fs#get_function_semantics in
+  mk_call_target_info api sem tgt
+
 let mk_unknown_target () =
   mk_default_target "unknown" UnknownTarget
 
 let mk_app_target (a:doubleword_int) =
-  mk_default_target a#to_hex_string (AppTarget a)
+  let finfo = get_function_info a in
+  let apsum = finfo#get_summary in
+  mk_function_summary_target apsum (AppTarget a)
 
 let mk_dll_target (dll:string) (name:string) =
-  mk_default_target name (StubTarget (DllFunction (dll,name)))
+  let tgt = StubTarget (DllFunction (dll,name)) in
+  if function_summary_library#has_dll_function dll name then
+    let fs = function_summary_library#get_dll_function dll name in
+    mk_function_summary_target fs tgt
+  else
+    mk_default_target name tgt
 
 let mk_so_target (name:string) =
-  mk_default_target name (StubTarget (SOFunction name))
+  let tgt = StubTarget (SOFunction name) in
+  if function_summary_library#has_so_function name then
+    let fs = function_summary_library#get_so_function name in
+    mk_function_summary_target fs tgt
+  else
+    mk_default_target name tgt
 
 let mk_jni_target (index:int) =
-  mk_default_target
-    ("jni_" ^ (string_of_int index))
-    (StubTarget (JniFunction index))
+  let tgt = StubTarget (JniFunction index) in
+  if function_summary_library#has_jni_function index then
+    let fs = function_summary_library#get_jni_function index in
+    mk_function_summary_target fs tgt
+  else
+    mk_default_target ("jni_" ^ (string_of_int index)) tgt
 
 let mk_virtual_target (fapi:function_api_t) =
   mk_default_target fapi.fapi_name (VirtualTarget fapi)
@@ -64,7 +86,12 @@ let mk_inlined_app_target (a:doubleword_int) (name:string) =
   mk_default_target name (InlinedAppTarget (a,name))
 
 let mk_static_dll_stub_target (a:doubleword_int) (dll:string) (name:string) =
-  mk_default_target name (StaticStubTarget(a, DllFunction (dll,name)))
+  let tgt = StaticStubTarget (a, DllFunction (dll,name)) in
+  if function_summary_library#has_dll_function dll name then
+    let fs = function_summary_library#get_dll_function dll name in
+    mk_function_summary_target fs tgt
+  else
+    mk_default_target name tgt
 
 let mk_static_pck_stub_target
       (a:doubleword_int) (lib:string) (pkgs:string list) (name:string) =
@@ -76,3 +103,8 @@ let mk_wrapped_target
       (tgt:call_target_t)
       (parameters:(api_parameter_t * bterm_t) list) =
   mk_default_target fapi.fapi_name ( WrappedTarget (a, fapi, tgt, parameters))
+
+let update_target_api (ctinfo:call_target_info_int) (fapi:function_api_t) =
+  let semantics = ctinfo#get_semantics in
+  let tgt = ctinfo#get_target in
+  mk_call_target_info fapi semantics tgt
