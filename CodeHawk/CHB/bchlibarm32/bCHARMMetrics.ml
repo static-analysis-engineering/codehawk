42a1463964713f770413edc94cd96df1f785d8e0
Henny Sipma <hennysipma@mac.com>
2021-04-05 21:15:02 -0700

    CHB-arm: structure to keep track of analysis progress

---
+++ CodeHawk/CHB/bchlibarm32/bCHARMMetrics.ml
@@ -0,0 +1,82 @@
+(* =============================================================================
+   CodeHawk Binary Analyzer 
+   Author: Henny Sipma and Andrew McGraw
+   ------------------------------------------------------------------------------
+   The MIT License (MIT)
+ 
+   Copyright (c) 2021 Aarno Labs LLC
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+ 
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+  
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+   ============================================================================= *)
+
+(* bchlib *)
+open BCHFloc
+open BCHLibTypes
+open BCHLocation
+open BCHMetricsHandler
+
+(* bchlibarm32 *)
+open BCHARMLoopStructure
+open BCHARMOpcodeRecords
+open BCHARMTypes
+
+let get_arm_op_metrics (f:arm_assembly_function_int) (finfo:function_info_int) =
+  let reads = ref 0 in
+  let qreads = ref 0 in
+  let writes = ref 0 in
+  let qwrites = ref 0 in
+  (!reads,!qreads,!writes,!qwrites)
+
+let get_arm_stackpointer_metrics (f:arm_assembly_function_int) (finfo:function_info_int) =
+  let faddr = f#get_address in
+  let esptop = ref 0 in
+  let esprange = ref 0 in
+  let _ =
+    f#iteri
+      (fun _ ctxtiaddr _ ->
+        let loc = ctxt_string_to_location faddr ctxtiaddr in
+        let floc = get_floc loc in
+        let (_,range) = floc#get_stackpointer_offset "arm" in
+        if range#isTop then
+          esptop := !esptop + 1
+        else match range#singleton with 
+               Some _ -> () | _ -> esprange := !esprange + 1) in
+  (!esptop,!esprange)
+
+
+let get_arm_memory_access_metrics (f:arm_assembly_function_int) (finfo:function_info_int) =
+  let (reads,qreads,writes,qwrites) = get_arm_op_metrics f finfo in
+  let (esptop,esprange) = get_arm_stackpointer_metrics f finfo in
+  { mmem_reads = reads ;
+    mmem_qreads = qreads ;
+    mmem_writes = writes ;
+    mmem_qwrites = qwrites ;
+    mmem_esptop = esptop ;
+    mmem_esprange = esprange
+  }
+
+let get_arm_cfg_metrics (f:arm_assembly_function_int) (env:function_environment_int) =
+  let _ = record_arm_loop_levels f#get_address in
+  { mcfg_instrs = f#get_instruction_count ;
+    mcfg_bblocks = f#get_block_count ;
+    mcfg_loops = get_arm_loop_count_from_table f ;
+    mcfg_loopdepth = get_arm_loop_depth_from_table f ;
+    mcfg_complexity = 0 ;
+    mcfg_vc_complexity = 0.0
+  }
