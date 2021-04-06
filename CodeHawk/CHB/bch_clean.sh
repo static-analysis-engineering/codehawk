a5c28b0dcb0892ec3652ab81d1d035a4f70ef2d3
Henny Sipma <hennysipma@mac.com>
2021-04-05 21:25:44 -0700

    CHB: scripts to build the binary analyzer

---
+++ CodeHawk/CHB/bch_clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+make -C bchlib clean
+make -C bchlibpe clean
+make -C bchlibelf clean
+make -C bchlibx86 clean
+make -C bchlibmips32 clean
+make -C bchlibarm32 clean
+make -C bchanalyze clean
+make -C bchcmdline clean
---
+++ CodeHawk/CHB/bch_make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+make -C bchlib
+make -C bchlibpe
+make -C bchlibelf
+make -C bchlibx86
+make -C bchlibmips32
+make -C bchlibarm32
+make -C bchanalyze
+make -C bchcmdline
