1f6e3def579f8d36ba6628a18b6cb8c080df28df
Henny Sipma <hennysipma@mac.com>
2021-06-26 21:16:01 -0700

    CHB: add mips metrics

--- CodeHawk/CHB/bchlibmips32/bCHMIPSMetrics.ml
+++ CodeHawk/CHB/bchlibmips32/bCHMIPSMetrics.ml
@@ -5,6 +5,8 @@
    The MIT License (MIT)
  
    Copyright (c) 2005-2019 Kestrel Technology LLC
+   Copyright (c) 2020      Henny Sipma
+   Copyright (c) 2021      Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -36,7 +38,8 @@ open BCHMIPSLoopStructure
 open BCHMIPSOpcodeRecords
 open BCHMIPSTypes
 
-let get_mips_op_metrics (f:mips_assembly_function_int) (finfo:function_info_int) =
+let get_mips_op_metrics
+      (f:mips_assembly_function_int) (finfo:function_info_int) =
   let faddr = f#get_address in
   let reads = ref 0 in
   let qreads = ref 0 in
