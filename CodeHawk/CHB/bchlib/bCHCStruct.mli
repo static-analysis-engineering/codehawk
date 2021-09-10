d68fd2a31c6948ce7f0f0a69fe3f1dc0dc41ee0a
Henny Sipma <hennysipma@mac.com>
2021-09-10 11:25:58 -0700

    CHB: minor changes

--- CodeHawk/CHB/bchlib/bCHCStruct.mli
+++ CodeHawk/CHB/bchlib/bCHCStruct.mli
@@ -5,6 +5,8 @@
    The MIT License (MIT)
  
    Copyright (c) 2005-2019 Kestrel Technology LLC
+   Copyright (c) 2020      Henny B. Sipma
+   Copyright (c) 2021      Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -33,6 +35,7 @@ open CHXmlDocument
 
 (* bchlib *)
 open BCHLibTypes
+
 
 val read_xml_c_struct: string -> xml_element_int -> c_struct_int
 val add_user_c_struct: string -> unit
