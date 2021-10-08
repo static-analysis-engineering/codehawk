2f47eac046eea09b9ef21b511249202ab9db5465
Henny Sipma <hennysipma@mac.com>
2021-10-08 14:25:53 -0700

    CHB:add option to analyze a given set of functions

--- CodeHawk/CHB/bchanalyze/bCHAnalyzeApp.mli
+++ CodeHawk/CHB/bchanalyze/bCHAnalyzeApp.mli
@@ -38,12 +38,16 @@ open BCHMIPSTypes
 
 val add_no_lineq: string -> unit
 val exclude_function: string -> unit
+val include_function: string -> unit
 
 val analyze_all: bool ref
 val analyze: float -> unit
 
 val analyze_x86_function: doubleword_int -> assembly_function_int -> unit
-val analyze_mips_function: doubleword_int -> mips_assembly_function_int -> int -> unit
-val analyze_mips: float -> unit
-val analyze_arm: float -> unit
 
+val analyze_mips_function:
+  doubleword_int -> mips_assembly_function_int -> int -> unit
+
+val analyze_mips: float -> unit
+
+val analyze_arm: float -> unit
