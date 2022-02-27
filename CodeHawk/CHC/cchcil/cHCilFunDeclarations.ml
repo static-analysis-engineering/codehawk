0a1e67308ca97f0abbfb7b4e3a8c2a5362fc243f
sipma <hennysipma@mac.com>
2022-02-27 12:45:59 -0800

    CHC-Cil: rename and update for goblint-cil

--- CodeHawk/CHC/cchcil/cHCilFunDeclarations.ml
+++ CodeHawk/CHC/cchcil/cHCilFunDeclarations.ml
@@ -5,6 +5,8 @@
    The MIT License (MIT)
  
    Copyright (c) 2005-2019 Kestrel Technology LLC
+   Copyright (c) 2020-2021 Henny Sipma
+   Copyright (c) 2022      Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -28,54 +30,68 @@
 (* cil *)
 open Cil
 
+(* chlib *)
+open CHPretty
+
+(* chutil *)
+open CHIndexTable
+open CHXmlDocument
+
 (* chcil *)
+open CHCilDictionary
+open CHCilSumTypeSerializer
 open CHCilTypes
-open CHDictionary
-open CHCIndexTable
-open CHPrettyPrint
-open CHSumTypeSerializer
-open CHXml
       
-let cd = CHDictionary.cdictionary
-let cdecls = CHDeclarations.cdeclarations
+let cd = CHCilDictionary.cildictionary
+let cdecls = CHCilDeclarations.cildeclarations
 
 let ibool b = if b then 1 else 0
 
-class cfundeclarations_t:cfundeclarations_int =
+class cilfundeclarations_t: cilfundeclarations_int =
 object (self)
 
   val local_varinfo_table = mk_index_table "local-varinfo-table"
   val mutable tables = []
 
   initializer
-    tables <- [ local_varinfo_table ]
+    tables <- [local_varinfo_table]
 
-  method private getrep (vinfo:varinfo) (formalseqnr:int) =
-    let tags = [ vinfo.vname; storage_serializer#to_string vinfo.vstorage ] in
-    let args = [ vinfo.vid; cd#index_typ vinfo.vtype; cd#index_attributes vinfo.vattr;
-                 ibool vinfo.vglob ; ibool vinfo.vinline; cdecls#index_location vinfo.vdecl;
-                 ibool vinfo.vaddrof ; formalseqnr ] in
-    (tags,args)
+  method private getrep (vinfo: varinfo) (formalseqnr: int) =
+    let tags = [vinfo.vname; storage_mfts#ts vinfo.vstorage] in
+    let args =
+      [vinfo.vid;
+       cd#index_typ vinfo.vtype;
+       cd#index_attributes vinfo.vattr;
+       ibool vinfo.vglob;
+       ibool vinfo.vinline;
+       cdecls#index_location vinfo.vdecl;
+       ibool vinfo.vaddrof;
+       formalseqnr] in
+    (tags, args)
 
-  method index_formal (vinfo:varinfo) (seqnr:int) =
+  method index_formal (vinfo: varinfo) (seqnr: int) =
     local_varinfo_table#add (self#getrep vinfo seqnr)
 
-  method index_local (vinfo:varinfo) =
+  method index_local (vinfo: varinfo) =
     local_varinfo_table#add (self#getrep vinfo 0)
 
-  method write_xml_formal ?(tag="f") (node:xml_element_int) (vinfo:varinfo) (seqnr:int) =
+  method write_xml_formal
+           ?(tag="f") (node: xml_element_int) (vinfo: varinfo) (seqnr: int) =
     node#setIntAttribute tag (self#index_formal vinfo seqnr)
 
-  method write_xml_local ?(tag="l") (node:xml_element_int) (vinfo:varinfo) =
+  method write_xml_local ?(tag="l") (node: xml_element_int) (vinfo: varinfo) =
     node#setIntAttribute tag (self#index_local vinfo)
 
-  method write_xml (node:xml_element_int) =
+  method write_xml (node: xml_element_int) =
     node#appendChildren
       (List.map (fun t ->
            let tnode = xmlElement t#get_name in
-           begin t#write_xml tnode ; tnode end) tables)
+           begin
+             t#write_xml tnode;
+             tnode
+           end) tables)
 
 end
 
-let mk_cfundeclarations () = new cfundeclarations_t
+let mk_cilfundeclarations () = new cilfundeclarations_t
     
--- CodeHawk/CHC/cchcil/cHCilFunDeclarations.mli
+++ CodeHawk/CHC/cchcil/cHCilFunDeclarations.mli
@@ -5,6 +5,8 @@
    The MIT License (MIT)
  
    Copyright (c) 2005-2019 Kestrel Technology LLC
+   Copyright (c) 2020-2021 Henny Sipma
+   Copyright (c) 2022      Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -28,4 +30,4 @@
 (* cchcil *)
 open CHCilTypes
 
-val mk_cfundeclarations: unit -> cfundeclarations_int
+val mk_cilfundeclarations: unit -> cilfundeclarations_int
