8fceb60733d3c9caeccd262c7bd1a51caeb9bb80
sipma <hennysipma@mac.com>
2022-02-27 12:40:57 -0800

    CHC-Cil: rename and update for goblint-cil

--- CodeHawk/CHC/cchcil/cHCilDictionary.mli
+++ CodeHawk/CHC/cchcil/cHCilDictionary.mli
@@ -5,6 +5,8 @@
    The MIT License (MIT)
  
    Copyright (c) 2005-2019 Kestrel Technology LLC
+   Copyright (c) 2020-2021 Henny Sipma
+   Copyright (c) 2022      Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -31,4 +33,4 @@ open Cil
 (* chcil *)
 open CHCilTypes
   
-val cdictionary: cdictionary_int
+val cildictionary: cildictionary_int
