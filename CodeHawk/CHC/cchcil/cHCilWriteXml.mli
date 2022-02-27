e75efb79065a98b793f4e16040ec712a0df534bc
sipma <hennysipma@mac.com>
2022-02-27 12:49:00 -0800

    CHC-Cil: rename and update for goblint-cil

--- CodeHawk/CHC/cchcil/cHCilWriteXml.mli
+++ CodeHawk/CHC/cchcil/cHCilWriteXml.mli
@@ -5,6 +5,8 @@
    The MIT License (MIT)
  
    Copyright (c) 2005-2019 Kestrel Technology LLC
+   Copyright (c) 2020-2021 Henny Sipma
+   Copyright (c) 2022      Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -28,8 +30,8 @@
 (* cil *)
 open Cil
 
-(* cchcil *)
-open CHXml
+(* chutil *)
+open CHXmlDocument
    
 val write_xml_function_definition: xml_element_int -> fundec -> string -> unit
 
