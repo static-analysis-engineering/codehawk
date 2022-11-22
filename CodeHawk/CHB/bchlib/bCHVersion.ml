4cf6d769beaa836130a9f61fd0415d4472d71d8e
Henny Sipma <hennysipma@mac.com>
2022-11-21 23:30:37 -0800

    Power: start adding VLE32 instructions

--- CodeHawk/CHB/bchlib/bCHVersion.ml
+++ CodeHawk/CHB/bchlib/bCHVersion.ml
@@ -93,7 +93,7 @@ end
 
 
 let version = new version_info_t 
-  ~version:"0.6.0_20221118"
-  ~date:"2022-11-18"
+  ~version:"0.6.0_20221121"
+  ~date:"2022-11-21"
   ~licensee: None
   ~maxfilesize: None
