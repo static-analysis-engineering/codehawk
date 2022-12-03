eed9a8228bfb0c96994a00a8dfa4541c5a27bb91
Henny Sipma <hennysipma@mac.com>
2022-12-03 15:27:31 -0800

    CHT: update command files for new directory structure

--- CodeHawk/CHT/tch_make.sh
+++ CodeHawk/CHT/tch_make.sh
@@ -2,9 +2,9 @@
 set -e
 make -C tchlib
 make -C CH_tests/xprlib_tests/txprlib
-make -C CH_tests/xprlib_tests/xprlib
+make -C CH_tests/xprlib_tests/txxprlib
 make -C CHB_tests/bchlib_tests/tbchlib
-make -C CHB_tests/bchlib_tests/bchlib
+make -C CHB_tests/bchlib_tests/txbchlib
 make -C CHB_tests/bchlibarm32_tests/tbchlibarm32
-make -C CHB_tests/bchlibarm32_tests/bchlibarm32
-make -C CHB_tests/bchlibpower32_tests/bchlibpower32
+make -C CHB_tests/bchlibarm32_tests/txbchlibarm32
+make -C CHB_tests/bchlibpower32_tests/txbchlibpower32
