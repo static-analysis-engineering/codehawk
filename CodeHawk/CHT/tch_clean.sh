dc75f549ea0ce4d1028f91faea36481eb37a54d9
Henny Sipma <hennysipma@mac.com>
2022-12-03 15:27:31 -0800

    CHT: update command files for new directory structure

--- CodeHawk/CHT/tch_clean.sh
+++ CodeHawk/CHT/tch_clean.sh
@@ -2,9 +2,9 @@
 set -e
 make -C tchlib clean
 make -C CH_tests/xprlib_tests/txprlib clean
-make -C CH_tests/xprlib_tests/xprlib clean
+make -C CH_tests/xprlib_tests/txxprlib clean
 make -C CHB_tests/bchlib_tests/tbchlib clean
-make -C CHB_tests/bchlib_tests/bchlib clean
+make -C CHB_tests/bchlib_tests/txbchlib clean
 make -C CHB_tests/bchlibarm32_tests/tbchlibarm32 clean
-make -C CHB_tests/bchlibarm32_tests/bchlibarm32 clean
-make -C CHB_tests/bchlibpower32_tests/bchlibpower32 clean
+make -C CHB_tests/bchlibarm32_tests/txbchlibarm32 clean
+make -C CHB_tests/bchlibpower32_tests/txbchlibpower32 clean
--- CodeHawk/CHT/tch_run.sh
+++ CodeHawk/CHT/tch_run.sh
@@ -2,9 +2,9 @@
 set -e
 make -C tchlib
 make -C CH_tests/xprlib_tests/txprlib
-make -C CH_tests/xprlib_tests/xprlib run
+make -C CH_tests/xprlib_tests/txxprlib run
 make -C CHB_tests/bchlib_tests/tbchlib
-make -C CHB_tests/bchlib_tests/bchlib run
+make -C CHB_tests/bchlib_tests/txbchlib run
 make -C CHB_tests/bchlibarm32_tests/tbchlibarm32
-make -C CHB_tests/bchlibarm32_tests/bchlibarm32 run
-make -C CHB_tests/bchlibpower32_tests/bchlibpower32 run
+make -C CHB_tests/bchlibarm32_tests/txbchlibarm32 run
+make -C CHB_tests/bchlibpower32_tests/txbchlibpower32 run
