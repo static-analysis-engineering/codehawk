4cdc4ef8250a8228599f56346269e1ee215bd18f
Henny Sipma <hennysipma@mac.com>
2022-12-19 16:38:16 -0800

    CHT:MIPS: add unit tests for instruction disassembly

--- CodeHawk/CHT/tch_clean.sh
+++ CodeHawk/CHT/tch_clean.sh
@@ -7,4 +7,5 @@ make -C CHB_tests/bchlib_tests/tbchlib clean
 make -C CHB_tests/bchlib_tests/txbchlib clean
 make -C CHB_tests/bchlibarm32_tests/tbchlibarm32 clean
 make -C CHB_tests/bchlibarm32_tests/txbchlibarm32 clean
+make -C CHB_tests/bchlibmips32_tests/txbchlibmips32 clean
 make -C CHB_tests/bchlibpower32_tests/txbchlibpower32 clean
--- CodeHawk/CHT/tch_make.sh
+++ CodeHawk/CHT/tch_make.sh
@@ -7,4 +7,5 @@ make -C CHB_tests/bchlib_tests/tbchlib
 make -C CHB_tests/bchlib_tests/txbchlib
 make -C CHB_tests/bchlibarm32_tests/tbchlibarm32
 make -C CHB_tests/bchlibarm32_tests/txbchlibarm32
+make -C CHB_tests/bchlibmips32_tests/txbchlibmips32
 make -C CHB_tests/bchlibpower32_tests/txbchlibpower32
