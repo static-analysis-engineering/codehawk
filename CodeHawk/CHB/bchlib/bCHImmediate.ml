d5c9f25739d08f3d14145491d92934898077fcaa
Henny Sipma <hennysipma@mac.com>
2023-03-12 19:41:21 -0700

    CHB: change base of immediate from big_val to numerical

--- CodeHawk/CHB/bchlib/bCHImmediate.ml
+++ CodeHawk/CHB/bchlib/bCHImmediate.ml
@@ -28,8 +28,9 @@
    ============================================================================= *)
 
 (* chlib *)
-open CHPretty
+open CHCommon
 open CHNumerical
+open CHPretty
 
 (* chutil *)
 open CHLogger
@@ -62,25 +63,25 @@ let _ =
           signedimmranges
           i
           (let p = (8 * i) - 1 in
-           let e = exp2 p in
-           (B.minus_big_int e, B.sub_big_int e B.unit_big_int));
+           let e = mkNumericalPowerOf2 p in
+           (numerical_zero#sub e, e));
         H.add
           unsignedimmranges
           i
           (let p = 8 * i in
-           let e = exp2 p in
-           (B.zero_big_int, B.sub_big_int e B.unit_big_int))
+           let e = mkNumericalPowerOf2 p in
+           (numerical_zero, e))
       end) [1; 2; 4; 8]
 
 
-let signed_imm_range (i: int): (B.big_int * B.big_int) TR.traceresult =
+let signed_imm_range (i: int): (numerical_t * numerical_t) TR.traceresult =
   if H.mem signedimmranges i then
     Ok (H.find signedimmranges i)
   else
     Error ["Immediate: byte size not supported: " ^ (string_of_int i)]
 
 
-let unsigned_imm_range (i: int): (B.big_int * B.big_int) TR.traceresult =
+let unsigned_imm_range (i: int): (numerical_t * numerical_t) TR.traceresult =
   if H.mem unsignedimmranges i then
     Ok (H.find unsignedimmranges i)
   else
@@ -88,16 +89,16 @@ let unsigned_imm_range (i: int): (B.big_int * B.big_int) TR.traceresult =
 
 
 class immediate_t
-        (signed:bool) (size_in_bytes:int) (big_val:B.big_int):immediate_int =
+        (signed:bool) (size_in_bytes:int) (num: numerical_t):immediate_int =
 object (self: 'a)
 
   val signed = signed
 
   val size_in_bytes = size_in_bytes
 
-  val big_val = big_val
+  val num = num
 
-  method equal (other:'a) = B.eq_big_int self#to_big_int other#to_big_int
+  method equal (other: 'a) = num#equal other#to_numerical
 
   (* ============================================================= Predicates *)
 
@@ -107,9 +108,7 @@ object (self: 'a)
 
   (* ============================================================= Converters *)
 
-  method to_big_int = big_val
-
-  method to_numerical = mkNumerical_big big_val
+  method to_numerical = num
 
   method to_doubleword =
     if size_in_bytes <= 4 then
@@ -118,8 +117,7 @@ object (self: 'a)
     else
       None
 
-  method to_int =
-    try Some (B.int_of_big_int big_val) with Failure _ -> None
+  method to_int = try Some num#toInt with CHFailure _ -> None
 
   (* =========================================================== Transformers *)
 
@@ -133,16 +131,21 @@ object (self: 'a)
           ^ " to:"
           ^ (string_of_int size)]
 
-  method to_unsigned = 
-    if B.ge_big_int big_val B.zero_big_int then
+  method to_unsigned =
+    if num#geq numerical_zero then
       {< signed = false >}
     else
-      {< signed = false ;
-         big_val = B.add_big_int big_val (exp2 (8*size_in_bytes)) >}
+      {< signed = false;
+         num = num#add (mkNumericalPowerOf2 (8 * size_in_bytes)) >}
+
+
+  method shift_left (n: int) = {< num = num#shift_left n >}
+
+  method arithmetic_shift_right (n: int) = {< num = num#shift_right n>}
 
   (* ======================================================== Pretty printing *)
 
-  method to_string = B.string_of_big_int big_val
+  method to_dec_string = num#toString
 
   method to_hex_string =
     if size_in_bytes <= 4 then
@@ -153,13 +156,13 @@ object (self: 'a)
         TR.tget_ok (numerical_to_hex_string numval)
     else
       if not signed then
-        let (hi, lo) = B.quomod_big_int big_val e32 in
-        if B.eq_big_int hi B.zero_big_int then
-          let dwlo = TR.tget_ok (big_int_to_doubleword lo) in
+        let (hi, lo) = num#quomod numerical_e32 in
+        if hi#equal numerical_zero then
+          let dwlo = TR.tget_ok (numerical_to_doubleword lo) in
           dwlo#to_hex_string
         else
-          let dwhi = TR.tget_ok (big_int_to_doubleword hi) in
-          let dwlo = TR.tget_ok (big_int_to_doubleword lo) in
+          let dwhi = TR.tget_ok (numerical_to_doubleword hi) in
+          let dwlo = TR.tget_ok (numerical_to_doubleword lo) in
           dwhi#to_hex_string ^ dwlo#to_fixed_length_hex_string
     else
       raise
@@ -168,29 +171,41 @@ object (self: 'a)
                 STR "Size for immediate not supported: ";
                 INT size_in_bytes]))
 
+  method to_string =
+    if num#geq numerical_zero then
+      if num#lt (mkNumerical 10) then
+        self#to_dec_string
+      else
+        self#to_hex_string
+    else
+      if num#gt (mkNumerical (-10)) then
+        self#to_dec_string
+      else
+        self#to_hex_string
+
   method toPretty =
     try
-      STR self#to_hex_string
+      STR self#to_string
     with
     | BCH_failure p ->
        begin
          ch_error_log#add
            "immediate value inconsistent"
            (LBLOCK [
-                STR (B.string_of_big_int big_val);
+                num#toPretty;
                 STR "; size in bytes: ";
                 INT size_in_bytes;
                 STR " (";
                 p;
                 STR ")"]);
-         STR (B.string_of_big_int big_val)
+         num#toPretty
        end
 
 end
 
 
 let make_immediate
-      (signed: bool) (size_in_bytes: int) (big_val: B.big_int): immediate_result =
+      (signed: bool) (size_in_bytes: int) (num: numerical_t): immediate_result =
   let immrange =
     if signed then
       signed_imm_range size_in_bytes
@@ -199,22 +214,23 @@ let make_immediate
   TR.tbind
     ~msg:"make_immediate"
     (fun (low, high) ->
-      if B.le_big_int low big_val && B.le_big_int big_val high then
-        Ok (new immediate_t signed size_in_bytes big_val)
+      if num#geq low && num#lt high then
+        (* if B.le_big_int low big_val && B.le_big_int big_val high then *)
+        Ok (new immediate_t signed size_in_bytes num)
       else
         Error [
             "Immediate value out-of-range: "
-            ^ (B.string_of_big_int big_val)
+            ^ num#toString
             ^ "(low: "
-            ^ (B.string_of_big_int low)
+            ^ low#toString
             ^ "; high: )"
-            ^ (B.string_of_big_int high)
+            ^ high#toString
             ^ ")"])
     immrange
 
 
 let signed_immediate_from_int ?(size=4) (i: int) =
-  make_immediate true size (B.big_int_of_int i)
+  make_immediate true size (mkNumerical i)
 
 let imm0 = TR.tget_ok (signed_immediate_from_int 0)
 let imm1 = TR.tget_ok (signed_immediate_from_int 1)
