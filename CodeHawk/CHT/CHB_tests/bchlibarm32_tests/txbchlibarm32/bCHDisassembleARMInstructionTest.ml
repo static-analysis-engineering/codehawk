50ec5cab5c917a7e067018c88f18b97c699eb8a4
Henny Sipma <hennysipma@mac.com>
2023-03-12 22:46:54 -0700

    CHT:ARM: add unit tests for ARM/Thumb disassembly

--- CodeHawk/CHT/CHB_tests/bchlibarm32_tests/txbchlibarm32/bCHDisassembleARMInstructionTest.ml
+++ CodeHawk/CHT/CHB_tests/bchlibarm32_tests/txbchlibarm32/bCHDisassembleARMInstructionTest.ml
@@ -61,7 +61,7 @@ module TR = CHTraceResult
 
 
 let testname = "bCHDisassembleARMInstructionTest"
-let lastupdated = "2023-01-17"
+let lastupdated = "2023-03-12"
 
 
 let make_dw (s: string) = TR.tget_ok (D.string_to_doubleword s)
@@ -90,27 +90,27 @@ let arm_basic () =
       ("ADDEQ",  "ff108102", "ADDEQ          R1, R1, #0xff");
       ("AND",    "1f0001e2", "AND            R0, R1, #0x1f");
       ("ASR",    "c10fa0e1", "ASR            R0, R1, #0x1f");
-      ("BIC",    "0320c2e3", "BIC            R2, R2, #0x3");
+      ("BIC",    "0320c2e3", "BIC            R2, R2, #3");
       ("BX",     "1eff2fe1", "BX             LR");
       ("BXNE",   "1eff2f11", "BXNE           LR");
       ("CLZ",    "111f6fe1", "CLZ            R1, R1");
-      ("CMN",    "010070e3", "CMN            R0, #0x1");
-      ("CMP",    "010050e3", "CMP            R0, #0x1");
-      ("CMPNE",  "00005111", "CMPNE          R1, R0,LSL #0");
+      ("CMN",    "010070e3", "CMN            R0, #1");
+      ("CMP",    "010050e3", "CMP            R0, #1");
+      ("CMPNE",  "00005111", "CMPNE          R1, R0");
       ("LDM",    "1c5091e8", "LDM            R1, {R2,R3,R4,R12,LR}");
-      ("LDR",    "001094e5", "LDR            R1, [R4, #0]");
-      ("LDRB",   "1000d5e5", "LDRB           R0, [R5, #16]");
-      ("LDRBNE", "1000d515", "LDRBNE         R0, [R5, #16]");
-      ("LDRD",   "d840c4e1", "LDRD           R4, R5, [R4, #8]");
+      ("LDR",    "001094e5", "LDR            R1, [R4]");
+      ("LDRB",   "1000d5e5", "LDRB           R0, [R5,#0x10]");
+      ("LDRBNE", "1000d515", "LDRBNE         R0, [R5,#0x10]");
+      ("LDRD",   "d840c4e1", "LDRD           R4, R5, [R4,#8]");
       ("LSL",    "1200a0e1", "LSL            R0, R2, R0");
-      ("LSR",    "a662a0e1", "LSR            R6, R6, #0x5");
+      ("LSR",    "a662a0e1", "LSR            R6, R6, #5");
       ("MOV",    "0140a0e1", "MOV            R4, R1");
-      ("MOVCC",  "0010a033", "MOVCC          R1, #0x0");
-      ("MOVT",   "050040e3", "MOVT           R0, #0x5");      
+      ("MOVCC",  "0010a033", "MOVCC          R1, #0");
+      ("MOVT",   "050040e3", "MOVT           R0, #5");
       ("MOVW",   "e40e05e3", "MOVW           R0, #0x5ee4");
       ("MOVWEQ", "ff100003", "MOVWEQ         R1, #0xff");
-      ("MOVWNE", "01000013", "MOVWNE         R0, #0x1");
-      ("ORR",    "046087e3", "ORR            R6, R7, #0x4");
+      ("MOVWNE", "01000013", "MOVWNE         R0, #1");
+      ("ORR",    "046087e3", "ORR            R6, R7, #4");
       ("POP",    "f08fbde8", "POP            {R4,R5,R6,R7,R8,R9,R10,R11,PC}");
       ("POPCC",  "108cbd38", "POPCC          {R4,R10,R11,PC}");
       ("POPEQ",  "f088bd08", "POPEQ          {R4,R5,R6,R7,R11,PC}");
@@ -122,8 +122,8 @@ let arm_basic () =
       ("QDSUB",  "513063e1", "QDSUB          R3, R1, R3");
       ("QSUB",   "555026e1", "QSUB           R5, R5, R6");
       ("REV",    "321fbfe6", "REV            R1, R2");
-      ("RSB",    "866366e0", "RSB            R6, R6, R6,LSL #7");
-      ("SBCS",   "0510d1e0", "SBCS           R1, R1, R5,LSL #0");
+      ("RSB",    "866366e0", "RSB            R6, R6, R6,LSL#7");
+      ("SBCS",   "0510d1e0", "SBCS           R1, R1, R5");
       ("SMLABB", "840800e1", "SMLABB         R0, R4, R8, R0");
       ("SMLABT", "c50800e1", "SMLABT         R0, R5, R8, R0");
       ("SMLATB", "ac3700e1", "SMLATB         R0, R12, R7, R3");
@@ -141,16 +141,16 @@ let arm_basic () =
       ("STM",    "1a0080e8", "STM            R0, {R1,R3,R4}");
       ("STMDA",  "030003e8", "STMDA          R3, {R0,R1}");
       ("STMIB",  "21078de9", "STMIB          SP, {R0,R5,R8,R9,R10}");
-      ("STR",    "38008de5", "STR            R0, [SP, #56]");
-      ("STRwb",  "08402de5", "STR            R4, [SP, -#8]!");
-      ("STRCC",  "00118337", "STRCC          R1, [R3, R0,LSL #2]");
-      ("STRB",   "0b00c4e5", "STRB           R0, [R4, #11]");
-      ("STRBEQ", "0300c405", "STRBEQ         R0, [R4, #3]");
-      ("STRD",   "f800c5e1", "STRD           R0, R1, [R5, #8]");
-      ("STRDwb", "fc406de1", "STRD           R4, R5, [SP, -#12]!");
-      ("STRH",   "b007cde1", "STRH           R0, [SP, #112]");
+      ("STR",    "38008de5", "STR            R0, [SP,#0x38]");
+      ("STRwb",  "08402de5", "STR            R4, [SP,-#8]!");
+      ("STRCC",  "00118337", "STRCC          R1, [R3,R0,LSL#2]");
+      ("STRB",   "0b00c4e5", "STRB           R0, [R4,#0xb]");
+      ("STRBEQ", "0300c405", "STRBEQ         R0, [R4,#3]");
+      ("STRD",   "f800c5e1", "STRD           R0, R1, [R5,#8]");
+      ("STRDwb", "fc406de1", "STRD           R4, R5, [SP,-#0xc]!");
+      ("STRH",   "b007cde1", "STRH           R0, [SP,#0x70]");
       ("SUB",    "45df4de2", "SUB            SP, SP, #0x114");
-      ("SUBS",   "062052e0", "SUBS           R2, R2, R6,LSL #0");
+      ("SUBS",   "062052e0", "SUBS           R2, R2, R6");
       ("TST",    "020c12e3", "TST            R2, #0x200");
       ("UBFX",   "50ede2e7", "UBFX           LR, R0, #26, #3");
       ("UMLAL",  "9324a5e0", "UMLAL          R2, R5, R3, R4");
@@ -205,6 +205,45 @@ let arm_pc_relative () =
   end
 
 
+let arm_vector () =
+  let tests = [
+      ("VDUP.32",                     "474cfcf3", "VDUP.32        Q10, D7[1]");
+      ("VDUP.32-scalar",              "622cfcf3", "VDUP.32        Q9, D18[1]");
+      ("VEOR-Q",                      "746106f3", "VEOR           Q3, Q3, Q10");
+      ("VEXT",                        "462cf0f2", "VEXT.8         Q9, Q0, Q3, #0xc");
+      ("VLD1.8",                      "0d0760f4", "VLD1.8         {D16}, [R0]!");
+      ("VLD1.32",                     "8f2a23f4", "VLD1.32        {D2,D3}, [R3]");
+      ("VMOV.I8",                     "584ec0f2", "VMOV.I8        Q10, #8");
+      ("VMOV.32-core-to-scalar",      "90cb25ee", "VMOV.32        D21[1], R12");
+      ("VMOV-R-A1",                   "502120f2", "VMOV           Q1, Q0");
+      ("VMULL.P64",                   "a94ea9f2", "VMULL.P64      Q2, D25, D25");
+      ("VPOP",                        "108bbdec", "VPOP           {D8,D9,D10,D11,D12,D13,D14,D15}");
+      ("VPUSH",                       "108b2bed", "VPUSH          {D8,D9,D10,D11,D12,D13,D14,D15}");
+      ("VSHL-I-I8",                   "522589f2", "VSHL.I8        Q1, Q1, #1");
+      ("VSHR.S32",                    "7220e1f2", "VSHR.S32       Q9, Q9, #0x1f");
+      ("VSHR.U64",                    "d640c1f3", "VSHR.U64       Q10, Q3, #0x3f");
+      ("VST1.8",                      "0f4a01f4", "VST1.8         {D4,D5}, [R1]");
+      ("VST1.32",                     "8d6a02f4", "VST1.32        {D6,D7}, [R2]!");
+      ("VST1.64",                     "cd8a40f4", "VST1.64        {D24,D25}, [R0]!");
+      ("VTBL",                        "0449f6f3", "VTBL.8         D20, {D6,D7}, D4");
+    ] in
+  begin
+    TS.new_testsuite (testname ^ "_arm_vector") lastupdated;
+
+    List.iter (fun (title, bytes, result) ->
+        TS.add_simple_test
+          ~title
+          (fun () ->
+            let ch = make_stream bytes in
+            let instrbytes = ch#read_doubleword in
+            let opcode = TF.disassemble_arm_instruction ch base instrbytes in
+            let opcodetxt = R.arm_opcode_to_string ~width:14 opcode in
+            A.equal_string result opcodetxt)) tests;
+
+    TS.launch_tests ()
+  end
+
+
 (* ARM-V8 crypto extension instructions *)
 let armv8_crypto () =
   let tests = [
@@ -235,6 +274,7 @@ let () =
     TS.new_testfile testname lastupdated;
     arm_basic ();
     arm_pc_relative ();
+    arm_vector ();
     armv8_crypto ();
     TS.exit_file ()
   end
