4c2da09cba0c03814119f55b32ba83643ec96cd6
Henny Sipma <hennysipma@mac.com>
2023-03-12 22:46:54 -0700

    CHT:ARM: add unit tests for ARM/Thumb disassembly

--- CodeHawk/CHT/CHB_tests/bchlibarm32_tests/txbchlibarm32/bCHDisassembleThumbInstructionTest.ml
+++ CodeHawk/CHT/CHB_tests/bchlibarm32_tests/txbchlibarm32/bCHDisassembleThumbInstructionTest.ml
@@ -61,7 +61,7 @@ module TR = CHTraceResult
 
 
 let testname = "bCHDisassembleThumbInstructionTest"
-let lastupdated = "2023-02-09"
+let lastupdated = "2023-03-12"
 
 
 let two_byte_instr_opcode_failures = [
@@ -90,20 +90,20 @@ let thumb_2_basic () =
   let tests = [
       ("ADCS-R-T1",  "4041", "ADCS           R0, R0, R0");
       ("ADD-SPI-T1", "04ac", "ADD            R4, SP, #0x10");
-      ("ADDS-I-T2",  "0132", "ADDS           R2, R2, #0x1");
+      ("ADDS-I-T2",  "0132", "ADDS           R2, R2, #1");
       ("ADDS-R-T2",  "7c44", "ADDS           R4, R4, PC");
       ("ADD-SPI-T2", "0ab0", "ADD            SP, SP, #0x28");
       ("AND-R-T1",   "1e40", "AND            R6, R6, R3");
-      ("ASRS-I-T1",  "4910", "ASRS           R1, R1, #0x1");
+      ("ASRS-I-T1",  "4910", "ASRS           R1, R1, #1");
       ("BLX-R-T1",   "9847", "BLX            R3");
       ("BX-T1",      "7047", "BX             LR");
       ("CMP-I-T1",   "102b", "CMP            R3, #0x10");
       ("CMP-R-T1",   "9a42", "CMP            R2, R3");
-      ("LDR-I-T2",   "0099", "LDR            R1, [SP, #0]");
-      ("LDRB-I-T1",  "9b78", "LDRB           R3, [R3, #2]");
-      ("LDRH-I-T1",  "1988", "LDRH           R1, [R3, #0]");
+      ("LDR-I-T2",   "0099", "LDR            R1, [SP]");
+      ("LDRB-I-T1",  "9b78", "LDRB           R3, [R3,#2]");
+      ("LDRH-I-T1",  "1988", "LDRH           R1, [R3]");
       ("LSLS-I-T1",  "c507", "LSLS           R5, R0, #0x1f");
-      ("LSRS-I-T1",  "9b08", "LSRS           R3, R3, #0x2");
+      ("LSRS-I-T1",  "9b08", "LSRS           R3, R3, #2");
       ("MOV-R-T1",   "1346", "MOV            R3, R2");
       ("MOVS-I-T1",  "2022", "MOVS           R2, #0x20");
       ("ORRS-R-T1",  "1e43", "ORRS           R6, R6, R3");
@@ -113,10 +113,10 @@ let thumb_2_basic () =
       ("PUSH-5-T1",  "f0b5", "PUSH           {R4,R5,R6,R7,LR}");
       ("REV-T1",     "02ba", "REV            R2, R0");
       ("REV16-T1",   "6dba", "REV16          R5, R5");
-      ("RSBS-I-T1",  "5b42", "RSBS           R3, R3, #0x0");
-      ("STR-I-T1",   "3560", "STR            R5, [R6, #0]");
-      ("STRB-I-T1",  "2170", "STRB           R1, [R4, #0]");
-      ("STRH-I-T1",  "8d80", "STRH           R5, [R1, #4]");
+      ("RSBS-I-T1",  "5b42", "RSBS           R3, R3, #0");
+      ("STR-I-T1",   "3560", "STR            R5, [R6]");
+      ("STRB-I-T1",  "2170", "STRB           R1, [R4]");
+      ("STRH-I-T1",  "8d80", "STRH           R5, [R1,#4]");
       ("SUB-SPI-T1", "85b0", "SUB            SP, SP, #0x14");
       ("SUBS-R-T1",  "ea1a", "SUBS           R2, R5, R3");
       ("SXTH-T1",    "36b2", "SXTH           R6, R6");
@@ -183,23 +183,24 @@ let thumb_2_pc_relative () =
 (* 4-byte thumb opcodes, not pc-relative *)
 let thumb_4_basic () =
   let tests = [
-      ("ADC.W-I-T2",  "43f1000c", "ADC.W          R12, R3, #0x0"); 
-      ("ADC.W-R-T2",  "4eeb0b0e", "ADC.W          LR, LR, R11,LSL #0");
+      ("ADC.W-I-T1",  "43f1000c", "ADC.W          R12, R3, #0");
+      ("ADC.W-R-T2",  "4eeb0b0e", "ADC.W          LR, LR, R11");
       ("ADD.W-I-T3",  "13f1ff33", "ADDS.W         R3, R3, #0xffffffff");
-      ("ADD.W-R-T3",  "0ceb8200", "ADD.W          R0, R12, R2,LSL #2");
+      ("ADD.W-R-T3",  "0ceb8200", "ADD.W          R0, R12, R2,LSL#2");
       ("AND-I-T1",    "01f47f03", "AND.W          R3, R1, #0xff0000");
       ("BIC-I-T1",    "23f0ff03", "BIC            R3, R3, #0xff");
       ("CLZ-T1",      "b3fa83f3", "CLZ            R3, R3");
       ("CMP.W-T2",    "b3f56c0f", "CMP.W          R3, #0xec0000");
       ("DMB-T1",      "bff35b8f", "DMB            ISH");
-      ("EOR.W-T2",    "80ea0c0c", "EOR.W          R12, R0, R12,LSL #0");
+      ("EOR.W-T2",    "80ea0c0c", "EOR.W          R12, R0, R12");
       ("LDM-T2",      "95e80f00", "LDM            R5, {R0,R1,R2,R3}");
-      ("LDR.W-T2",    "5cf82230", "LDR.W          R3, [R12, R2,LSL #2]");
-      ("LDRB.W-T2",   "9ef81040", "LDRB.W         R4, [LR, #16]");
-      ("LDRD-T1",     "dee90201", "LDRD           R0, R1, [LR, #8]");
-      ("LDREX-T1",    "54e8002f", "LDREX          R2, [R4, #0]");
-      ("LDRH.W-I-T2", "b2f84d20", "LDRH.W         R2, [R2, #77]");
-      ("LSR.W-I-T2",  "4fea5002", "LSR.W          R2, R0, #0x1");
+      ("LDR.W-T2",    "5cf82230", "LDR.W          R3, [R12,R2,LSL#2]");
+      ("LDRB.W-T2",   "9ef81040", "LDRB.W         R4, [LR,#0x10]");
+      ("LDRD-T1-off", "dee90201", "LDRD           R0, R1, [LR,#8]");
+      ("LDRD-T1-post","f4e80201", "LDRD           R0, R1, [R4],#8");
+      ("LDREX-T1",    "54e8002f", "LDREX          R2, [R4]");
+      ("LDRH.W-I-T2", "b2f84d20", "LDRH.W         R2, [R2,#0x4d]");
+      ("LSR.W-I-T2",  "4fea5002", "LSR.W          R2, R0, #1");
       ("MLS-T1",      "0efb1202", "MLS            R2, LR, R2, R0");
       ("MOVT-T1",     "c8f20214", "MOVT           R4, #0x8102");
       ("MOVW-I-T3",   "40f20944", "MOVW           R4, #0x409");
@@ -207,21 +208,27 @@ let thumb_4_basic () =
       ("MVN-T1",      "6ff06300", "MVN            R0, #0x63");
       ("NOP-T2",      "aff30080", "NOP           ");
       ("ORR-I-T1",    "46f00046", "ORR            R6, R6, #0x80000000");
+      ("PLD-I",       "90f840f0", "PLD            [R0,#0x40]");
       ("POP.W-T2",    "bde83840", "POP.W          {R3,R4,R5,LR}");
       ("PUSH.W-T2",   "2de9f041", "PUSH.W         {R4,R5,R6,R7,R8,LR}");
-      ("RSB.W-T2",    "c1f10001", "RSB.W          R1, R1, #0x0");
+      ("RSB.W-T2",    "c1f10001", "RSB.W          R1, R1, #0");
       ("STM.W-T2",    "84e80f00", "STM.W          R4, {R0,R1,R2,R3}");
-      ("STMDB-T1",    "04e90f00", "STMDB.W        R4, {R0,R1,R2,R3}");
-      ("STR.W-I-T4",  "41f8206f", "STR.W          R6, [R1, #32]!");
-      ("STR.W-R-T2",  "4cf82630", "STR.W          R3, [R12, R6,LSL #2]");
-      ("STRB.W-T3",   "83f81044", "STRB.W         R4, [R3, #1040]");
-      ("STRD-T1",     "cde90023", "STRD           R2, R3, [SP, #0]");
-      ("STREX-T1",    "44e80031", "STREX          R1, R3, [R4, #0]");
-      ("STRH.W-T2",   "a4f84430", "STRH.W         R3, [R4, #68]");
+      ("STMDB-T1",    "04e90f00", "STMDB          R4, {R0,R1,R2,R3}");
+      ("STR.W-I-T4",  "41f8206f", "STR.W          R6, [R1,#0x20]!");
+      ("STR.W-R-T2",  "4cf82630", "STR.W          R3, [R12,R6,LSL#2]");
+      ("STRB.W-T3",   "83f81044", "STRB.W         R4, [R3,#0x410]");
+      ("STRD-T1",     "cde90023", "STRD           R2, R3, [SP]");
+      ("STREX-T1",    "44e80031", "STREX          R1, R3, [R4]");
+      ("STRH.W-T2",   "a4f84430", "STRH.W         R3, [R4,#0x44]");
       ("SUB.W-T3",    "a6f17f01", "SUB.W          R1, R6, #0x7f");
-      ("TBB-T1",      "dfe800f0", "TBB            [PC, R0]");
+      ("TBB-T1",      "dfe800f0", "TBB            [PC,R0]");
       ("UBFX-T1",     "c0f30743", "UBFX           R3, R0, #16, #8");
-      ("UMULL-T1",    "a4fb0232", "UMULL          R3, R2, R4, R2")
+      ("UMLAL",       "e8fb0a23", "UMLAL          R2, R3, R8, R10");
+      ("UMULL-T1",    "a4fb0232", "UMULL          R3, R2, R4, R2");
+      ("UQSUB8",      "ccfa52f4", "UQSUB8         R4, R12, R2");
+      ("UXTAB",       "52fa81f5", "UXTAB          R5, R2, R1");
+      ("UXTB.W",      "5ffa83fe", "UXTB.W         LR, R3");
+      ("UXTH.W",      "1ffa83f8", "UXTH.W         R8, R3");
     ] in
   begin
     TS.new_testsuite (testname ^ "_thumb_4_basic") lastupdated;
@@ -277,13 +284,37 @@ let thumb_4_pc_relative () =
 (* 4-byte thumb vector instructions *)
 let thumb_4_vector () =
   let tests = [
-      ("VMCPE.F64-T1-double",         "b4eec07b", "VCMPE.F64      D7, D0");
+      ("FLDMIAX-T1",                  "90ec210b", "FLDMIAX        R0, {D0,D1,D2,D3,D4,D5,D6,D7,D8,D9,D10,D11,D12,D13,D14,D15}");
+      ("VADD-I-T1",                   "72efee28", "VADD.I64       Q9, Q9, Q15");
+      ("VADDL-u-T1",                  "aaff2ea0", "VADDL.U32      Q5, D10, D30");
+      ("VAND-T1",                     "40eff201", "VAND           Q8, Q8, Q9");
+      ("VBIC-I-T1-1",                 "c7ff3c07", "VBIC.I32       D16, #0xfc000000");
+      ("VCMPE.F64-T1-double",         "b4eec07b", "VCMPE.F64      D7, D0");
+      ("VCMPE.F64.T2-double",         "b5eec00b", "VCMPE.F64      D0, #0.0");
       ("VCVT.F32.U32-single-from-T1", "b8ee470a", "VCVT.F32.U32   S0, S14");
       ("VCVT.F64.F32-ds-T1",          "b7eec00a", "VCVT.F64.F32   D0, S0");
       ("VCVT.F64.S32-double-from-T1", "b8eee77b", "VCVT.F64.S32   D7, S15");
-      ("VDIV.F32-T1",                 "80ee270a", "VDIV.F32       S0, S0, S15");
+      ("VDIV.F32",                    "80ee270a", "VDIV.F32       S0, S0, S15");
+      ("VDIV.F64",                    "87ee067b", "VDIV.F64       D7, D7, D6");
+      ("VDUP.8-scalar",               "ffff472c", "VDUP.8         Q9, D7[7]");
+      ("VEOR-Q",                      "06ff7061", "VEOR           Q3, Q3, Q8");
+      ("VLD1.8",                      "6cf90d82", "VLD1.8         {D24,D25,D26,D27}, [R12]!");
+      ("VLDM-T1",                     "90ec200b", "VLDM           R0, {D0,D1,D2,D3,D4,D5,D6,D7,D8,D9,D10,D11,D12,D13,D14,D15}");
+      ("VLDR-imm",                    "dded108b", "VLDR           D24, [SP,#0x40]");
+      ("VMLAL.U32-by-scalar-T2",      "a7ff62a2", "VMLAL.U32      Q5, D7, D2[1]");
       ("VMOV-core-to-sp-T1",          "07ee907a", "VMOV           S15, R7");
+      ("VMOVN.I64",                   "faff2002", "VMOVN.I64      D16, Q8");
       ("VMRS-T1",                     "f1ee10fa", "VMRS           APSR_nzcv, FPSCR");
+      ("VMSR-T1",                     "e1ee102a", "VMSR           FPSCR, R2");
+      ("VMULL.U32-by-scalar-T2",      "a0ff60aa", "VMULL.U32      Q5, D0, D0[1]");
+      ("VORR-R-D",                    "27ef3a71", "VORR           D7, D7, D26");
+      ("VSHL.I32",                    "e2ef3ee5", "VSHL.I32       D30, D30, #2");
+      ("VSHR.S8",                     "c9ef7220", "VSHR.S8        Q9, Q9, #7");
+      ("VSHR.U32",                    "e6ff1ae0", "VSHR.U32       D30, D10, #0x1a");
+      ("VSHR.U64",                    "a6ffda80", "VSHR.U64       Q4, Q5, #0x1a");
+      ("VSHRN.I64",                   "e6ef32e8", "VSHRN.I64      D30, Q9, #0x1a");
+      ("VTRN.32",                     "baff8a00", "VTRN.32        D0, D10");
+      ("VZIP.16",                     "f6ff88c1", "VZIP.16        D28, D8");
     ] in
   begin
     TS.new_testsuite (testname ^ "_thumb_4_vector") lastupdated;
