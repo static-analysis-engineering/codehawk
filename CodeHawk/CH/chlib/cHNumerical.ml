c5d42105467f5c83cc0a09a305a3a9bb4c8ce61c
Henny Sipma <hennysipma@mac.com>
2023-03-12 18:40:07 -0700

    CH:add functionality to numerical_t

--- CodeHawk/CH/chlib/cHNumerical.ml
+++ CodeHawk/CH/chlib/cHNumerical.ml
@@ -5,6 +5,8 @@
    The MIT License (MIT)
  
    Copyright (c) 2005-2019 Kestrel Technology LLC
+   Copyright (c) 2020-2021 Henny Sipma
+   Copyright (c) 2022-2023 Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -31,6 +33,7 @@ open Big_int_Z
 open CHCommon
 open CHPretty
 
+
 class numerical_t n =
 object (self: 'a)
 
@@ -51,21 +54,35 @@ object (self: 'a)
       self#mkNew (div_big_int num n#getNum)
     with
     | Division_by_zero ->
-       raise (CHFailure (LBLOCK [ STR "Numerical division by zero: " ;
-                                  self#toPretty ; STR " / " ; n#toPretty ]))
+       raise
+         (CHFailure
+            (LBLOCK [
+                 STR "Numerical division by zero: ";
+                 self#toPretty;
+                 STR " / ";
+                 n#toPretty]))
 
   method modulo (n: 'a) =
     try
       self#mkNew (mod_big_int num n#getNum)
     with
     | Division_by_zero ->
-       raise (CHFailure (LBLOCK [ STR "Numerical division by zero (modulo): " ;
-                                  self#toPretty ; STR " % " ; n#toPretty ]))
+       raise
+         (CHFailure
+            (LBLOCK [
+                 STR "Numerical division by zero (modulo): ";
+                 self#toPretty;
+                 STR " % ";
+                 n#toPretty]))
+
+  method quomod (n: 'a) =
+    let (quo, modd) = quomod_big_int num n#getNum in
+    (self#mkNew quo, self#mkNew modd)
 
   method gcd (n: 'a) =
     let zero = self#mkNew (big_int_of_int 0) in
     if self#equal zero && n#equal zero then
-      raise (CHFailure (LBLOCK [ STR "Gcd(0,0) is not defined" ]))
+      raise (CHFailure (LBLOCK [STR "Gcd(0,0) is not defined"]))
     else
       if self#equal zero then
         n
@@ -76,16 +93,45 @@ object (self: 'a)
         self#mkNew (gcd_big_int num n#getNum)
       with
       | Division_by_zero ->
-         raise (CHFailure (LBLOCK [ STR "Numerical division by zero (gcd): " ;
-                                    self#toPretty ; STR " gcd " ; n#toPretty ]))
+         raise
+           (CHFailure
+              (LBLOCK [
+                   STR "Numerical division by zero (gcd): ";
+                   self#toPretty;
+                   STR " gcd ";
+                   n#toPretty]))
 
   method lcm (n: 'a) =
     try
       self#mkNew (div_big_int (mult_big_int num n#getNum) (gcd_big_int num n#getNum))
     with
     | Division_by_zero ->
-       raise (CHFailure (LBLOCK [ STR "Numerical division by z ero (lcm): " ;
-                                  self#toPretty ; STR " lcm " ; n#toPretty ]))
+       raise
+         (CHFailure
+            (LBLOCK [
+                 STR "Numerical division by z ero (lcm): ";
+                 self#toPretty;
+                 STR " lcm ";
+                 n#toPretty]))
+
+  method shift_left (n: int): 'a =
+    if n >= 0 then
+      self#mkNew (shift_left_big_int num n)
+    else
+      raise
+        (CHFailure
+           (LBLOCK [
+                STR "Numerical shift left with negative shift amount: "; INT n]))
+
+  (* arithmetic shift *)
+  method shift_right (n: int): 'a =
+    if n >= 0 then
+      self#mkNew (shift_right_big_int num n)
+    else
+      raise
+        (CHFailure
+           (LBLOCK [
+                STR "Numerical shift right with negative shift amount: "; INT n]))
 
   method leq (n: 'a) = le_big_int num n#getNum
 
@@ -116,23 +162,35 @@ object (self: 'a)
   method toInt = 
     try
       int_of_big_int num
-    with Failure _ ->
-      raise (CHFailure (LBLOCK [STR "Numerical to int conversion failure: ";
-                                self#toPretty]))
+    with
+    | Failure _ ->
+       raise
+         (CHFailure
+            (LBLOCK [
+                 STR "Numerical to int conversion failure: ";
+                 self#toPretty]))
 
   method toInt64 =
     try
       Int64.of_string self#toString
-    with Failure _ ->
-      raise (CHFailure (LBLOCK [ STR "Numerical to int64 conversion failure: " ;
-                                 self#toPretty ]))
+    with
+    | Failure _ ->
+       raise
+         (CHFailure
+            (LBLOCK [
+                 STR "Numerical to int64 conversion failure: ";
+                 self#toPretty]))
 
   method toInt32 =
     try
       Int32.of_string self#toString
-    with Failure _ ->
-      raise (CHFailure (LBLOCK [ STR "Numerical to int32 conversion failure: " ;
-                                 self#toPretty ]))
+    with
+    | Failure _ ->
+       raise
+         (CHFailure
+            (LBLOCK [
+                 STR "Numerical to int32 conversion failure: ";
+                 self#toPretty]))
 
   method toRational =
     new rational_t num
@@ -144,7 +202,7 @@ object (self: 'a)
 
   val num = Q.of_bigint n
 
-  method getNum = num
+  method getNum: Q.t = num
 
   method private mkNew n = {< num = n >}
 
@@ -159,20 +217,25 @@ object (self: 'a)
       self#mkNew (Q.div num n#getNum)
     with
     | Division_by_zero ->
-       raise (CHFailure (LBLOCK [ STR "Rational division by zero: " ;
-                                  self#toPretty ; STR " / "  ; n#toPretty ]))
+       raise
+         (CHFailure
+            (LBLOCK [
+                 STR "Rational division by zero: ";
+                 self#toPretty;
+                 STR " / ";
+                 n#toPretty]))
 
-  method leq (n: 'a) = Q.leq num n#getNum
+  method leq (n: 'a): bool = Q.leq num n#getNum
 
-  method lt (n: 'a) = Q.lt num n#getNum
+  method lt (n: 'a): bool = Q.lt num n#getNum
 
-  method geq (n: 'a) = Q.geq num n#getNum
+  method geq (n: 'a): bool = Q.geq num n#getNum
 
-  method gt (n: 'a) = Q.gt num n#getNum
+  method gt (n: 'a): bool = Q.gt num n#getNum
 
-  method equal (n: 'a) = Q.equal num n#getNum
+  method equal (n: 'a): bool = Q.equal num n#getNum
 
-  method compare (n: 'a) = Q.compare num n#getNum
+  method compare (n: 'a): int = Q.compare num n#getNum
 
   method toString = Q.to_string num
 
@@ -204,11 +267,30 @@ object (self: 'a)
 
 end
 
+
 let mkNumerical n = new numerical_t (big_int_of_int n)
 
 let numerical_zero = mkNumerical 0
 
 let numerical_one = mkNumerical 1
+
+
+let mkNumericalPowerOf2 (n: int) =
+  new numerical_t (power_int_positive_int 2 n)
+
+
+let numerical_e8 = mkNumerical 256
+
+let numerical_e15 = mkNumericalPowerOf2 15
+
+let numerical_e16 = mkNumericalPowerOf2 16
+
+let numerical_e31 = mkNumericalPowerOf2 31
+
+let numerical_e32 = mkNumericalPowerOf2 32
+
+let numerical_e64 = mkNumericalPowerOf2 64
+
 
 let mkNumerical_big n = new numerical_t n
 
@@ -216,6 +298,45 @@ let mkNumericalFromString s = new numerical_t (big_int_of_string s)
 
 let mkRational n = new rational_t (big_int_of_int n)
 
-let mkNumericalFromInt32 (n: int32) = new numerical_t (big_int_of_string (Int32.to_string n))
 
-let mkNumericalFromInt64 (n: int64) = new numerical_t (big_int_of_string (Int64.to_string n))
+let mkNumericalFromInt32 (n: int32) =
+  new numerical_t (big_int_of_string (Int32.to_string n))
+
+
+let mkNumericalFromInt64 (n: int64) =
+  new numerical_t (big_int_of_string (Int64.to_string n))
+
+
+let mkNumericalFromBitString s =
+  if (String.length s) > 64 then
+    raise
+      (CHFailure
+         (LBLOCK [
+              STR "mkNumericalFromBitString: string is too long: ";
+              INT (String.length s);
+              STR " (currently only supports strings less than or equal to 64 chars"]))
+  else if (String.length s) = 64 then
+    try
+      let s1 = String.sub s 0 32 in
+      let s2 = String.sub s 32 32 in
+      let i1 = Int64.of_string("0b" ^ s1) in
+      let i2 = Int64.of_string("0b" ^ s2) in
+      let n1 = mkNumericalFromInt64 i1 in
+      let n2 = mkNumericalFromInt64 i2 in
+      (n1#mult numerical_e32)#add n2
+    with
+    | Failure _ ->
+       raise
+         (CHFailure
+            (LBLOCK [
+                 STR "Error in 64-bit string to numerical conversion: ";
+                 STR s]))
+  else
+    try
+      let i64 = Int64.of_string ("0b" ^ s) in
+      mkNumericalFromInt64 i64
+    with
+    | Failure _ ->
+       raise
+         (CHFailure
+            (LBLOCK [STR "Error in bitstring to numerical conversion: "; STR s]))
--- CodeHawk/CH/chlib/cHNumerical.mli
+++ CodeHawk/CH/chlib/cHNumerical.mli
@@ -5,6 +5,8 @@
    The MIT License (MIT)
  
    Copyright (c) 2005-2019 Kestrel Technology LLC
+   Copyright (c) 2020-2021 Henny Sipma
+   Copyright (c) 2022-2023 Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -47,9 +49,12 @@ class numerical_t :
     method lt : 'a -> bool
     method private mkNew : big_int -> 'a
     method modulo : 'a -> 'a
+    method quomod : 'a -> 'a * 'a
     method mult : 'a -> 'a
     method neg : 'a
     method sgn : int
+    method shift_left: int -> 'a
+    method shift_right: int -> 'a
     method sub : 'a -> 'a
     method toInt : int
     method toInt32 : Int32.t
@@ -81,18 +86,65 @@ and rational_t :
     method toString : string
   end
 
-val mkNumerical : int -> numerical_t
 
+(** Numerical for 0 *)
 val numerical_zero : numerical_t
 
+
+(** Numerical for 1 *)
 val numerical_one : numerical_t
 
+
+(** Numerical for 2^8 (256) *)
+val numerical_e8: numerical_t
+
+
+(** Numerical for 2^15 *)
+val numerical_e15: numerical_t
+
+
+(** Numerical for 2^16 *)
+val numerical_e16: numerical_t
+
+
+(** Numerical for 2^31 *)
+val numerical_e31: numerical_t
+
+
+(** Numerical for 2^32 *)
+val numerical_e32: numerical_t
+
+
+(** Numerical for 2^64 *)
+val numerical_e64: numerical_t
+
+
+(** [mkNumerical i] returns the numerical for value [i]. *)
+val mkNumerical : int -> numerical_t
+
+
+(** [mkNumerical i] returns the numerical for value [i]. *)
 val mkNumerical_big : big_int -> numerical_t
 
+
+(** [mkRational r] returns the rational for value [r]. *)
 val mkRational : int -> rational_t
+
 
 val mkNumericalFromInt32 : int32 -> numerical_t
 
 val mkNumericalFromInt64 : int64 -> numerical_t
 
 val mkNumericalFromString: string -> numerical_t
+
+
+(** [mkNumericalPowerOf2 n] returns the numerical for 2^n] *)
+val mkNumericalPowerOf2: int -> numerical_t
+
+
+(** [mkNumericalFromBitString s] returns the numerical for the value represented
+    by the bitstring [s]. [s] must be a string consisting solely of 0's and 1's
+    (no leading 0b).
+
+    Maximumum length currently supported is 64 characters.*)
+val mkNumericalFromBitString: string -> numerical_t
