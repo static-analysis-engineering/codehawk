17f521d2462fe1c9b9099d55dfde0a7cdf63d5f4
Henny Sipma <hennysipma@mac.com>
2023-04-06 19:25:07 -0700

    CHT:DWARF: update location data types

--- CodeHawk/CHT/CHB_tests/bchlibelf_tests/tbchlibelf/tCHBchlibelfAssertion.ml
+++ CodeHawk/CHT/CHB_tests/bchlibelf_tests/tbchlibelf/tCHBchlibelfAssertion.ml
@@ -69,8 +69,9 @@ let equal_abbrev_entry
 let equal_variable_debuginfo_entry
       ?(msg="")
       ~(expected: (dwarf_attr_type_t * string) list)
-      ~(received: debug_info_entry_t) =
-  let recvvalues = received.ie_values in
+      ~(received: debug_info_entry_t)
+      ()=
+  let recvvalues = received.dwie_values in
   A.make_equal_list
     (fun (xat, xv) (rat, rv) -> (xat = rat) && (xv = rv))
     (fun (att, v) -> ((dwarf_attr_type_to_string att) ^ ": " ^ v))
@@ -82,7 +83,8 @@ let equal_variable_debuginfo_entry
 let equal_debug_location_list
       ?(msg="")
       ~(expected: (string * string * string) list)
-      ~(received: debug_location_list_entry_t list) =
+      ~(received: debug_location_list_entry_t list)
+      ()=
   A.make_equal_list
     (fun (xsa, xea, xx) (rsa, rea, rx) ->
       (xsa = rsa) && (xea = rea) && (xx = rx))
@@ -95,8 +97,8 @@ let equal_debug_location_list
             (l.lle_start_address#to_hex_string,
              l.lle_end_address#to_hex_string,
              single_location_description_to_string l.lle_location)
-         | BaseAddressSelectionEntry dw -> (dw#to_hex_string, "", "")
-         | EndOfListEntry -> ("", "", "")) received)
+         | LocBaseAddressSelectionEntry dw -> (dw#to_hex_string, "", "")
+         | LocEndOfListEntry -> ("", "", "")) received)
 
 
 let equal_compilation_unit_header
@@ -140,12 +142,12 @@ let equal_compilation_unit
     A.fail xlen hdr.dwcu_length#to_hex_string msg
   else if not (xoffset = hdr.dwcu_offset#to_hex_string) then
     A.fail xoffset hdr.dwcu_offset#to_hex_string msg
-  else if not (xnr = cu.ie_abbrev) then
-    A.fail (string_of_int xnr) (string_of_int cu.ie_abbrev) msg
-  else if not (xtag = cu.ie_tag) then
+  else if not (xnr = cu.dwie_abbrev) then
+    A.fail (string_of_int xnr) (string_of_int cu.dwie_abbrev) msg
+  else if not (xtag = cu.dwie_tag) then
     A.fail
       (dwarf_tag_type_to_string xtag)
-      (dwarf_tag_type_to_string cu.ie_tag)
+      (dwarf_tag_type_to_string cu.dwie_tag)
       msg
   else
     A.make_equal_list
@@ -157,4 +159,4 @@ let equal_compilation_unit
            (dwarf_attr_type_to_string attr, attrv)) attrs)
       (List.map (fun (attr, attrv) ->
            (dwarf_attr_type_to_string attr,
-            dwarf_attr_value_to_string attrv)) cu.ie_values)
+            dwarf_attr_value_to_string attrv)) cu.dwie_values)
