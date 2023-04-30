92f062f9b2ecebd0f863ae1daf39c2485a43a613
Henny Sipma <hennysipma@mac.com>
2023-04-30 16:03:26 -0700

    CHB:PWR:conditional expressions and loops

---
+++ CodeHawk/CHB/bchlibpower32/bCHPowerConditionalExpr.ml
@@ -0,0 +1,157 @@
+(* =============================================================================
+   CodeHawk Binary Analyzer 
+   Author: Henny Sipma
+   ------------------------------------------------------------------------------
+   The MIT License (MIT)
+ 
+   Copyright (c) 2023  Aarno Labs LLC
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+ 
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+  
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+   ============================================================================= *)
+
+(* chlib *)
+open CHLanguage
+open CHNumerical
+open CHPretty
+open CHUtils
+
+(* chutil *)
+open CHLogger
+
+(* xprlib *)
+open Xprt
+open XprTypes
+open XprToPretty
+open XprUtil
+open Xsimplify
+
+(* bchlib *)
+open BCHBasicTypes
+open BCHDoubleword
+open BCHFloc
+open BCHFunctionInfo
+open BCHLibTypes
+open BCHLocation
+open BCHSystemSettings
+open BCHVariable
+
+(* bchlibpower32 *)
+open BCHPowerAssemblyInstruction
+open BCHPowerOperand
+open BCHPowerOpcodeRecords
+open BCHPowerTypes
+
+
+let x2p = xpr_formatter#pr_expr
+
+
+let tracked_locations = []
+
+
+let track_location loc p =
+  if List.mem loc tracked_locations then
+    ch_diagnostics_log#add ("tracked:" ^ loc) p
+
+
+let freeze_variables
+      ?(unsigned=false)
+      (add: variable_t -> variable_t -> unit) 
+      (testloc: location_int)
+      (condloc: location_int)
+      (op: pwr_operand_int)  =
+  let testfloc = get_floc testloc in
+  let condfloc = get_floc condloc in
+  let env = testfloc#f#env in
+  let opXpr = op#to_expr testfloc in
+  let frozenVars = new VariableCollections.table_t in
+  let vars = (variables_in_expr opXpr) in
+  let varsKnown = ref true in
+  let _ =
+    List.iter (
+        fun v -> 
+        if v#isTmp then
+          varsKnown := false
+        else if env#is_function_initial_value v then
+          ()
+        else if env#is_local_variable v then
+          let _ =
+            track_location
+              testloc#ci
+              (LBLOCK [
+                   v#toPretty; NL;
+                   testfloc#inv#toPretty; NL;
+                   condfloc#inv#toPretty]) in
+          if condfloc#inv#test_var_is_equal v testloc#ci condloc#ci then
+            let _ =
+              track_location
+                condloc#ci
+                (LBLOCK [
+                     v#toPretty; NL;
+                     STR " test_var_is_equal"]) in
+            ()
+          else
+            let fv = env#mk_frozen_test_value v testloc#ci condloc#ci in
+            frozenVars#set v fv
+        else if env#is_unknown_memory_variable v then
+          varsKnown := false) vars in
+  let subst v =
+    if frozenVars#has v then
+      match testfloc#inv#get_external_exprs v with
+      | x::_ -> x
+      | _ -> XVar (Option.get (frozenVars#get v))
+    else
+      XVar v in
+  if !varsKnown then
+    begin
+      List.iter (fun (v, fv) -> add v fv) frozenVars#listOfPairs ;
+      substitute_expr subst opXpr
+    end
+  else
+    random_constant_expr
+        
+
+let pwr_conditional_expr
+      ~(condopc: pwr_opcode_t)
+      ~(testopc: pwr_opcode_t)
+      ~(condloc: location_int)
+      ~(testloc: location_int) =
+  let frozenVars = new VariableCollections.table_t in
+  let add v fv = frozenVars#set v fv in
+  let v = freeze_variables add testloc condloc in
+  let vu = freeze_variables ~unsigned:true add testloc condloc in
+  let testfloc = get_floc testloc in
+  let condfloc = get_floc condloc in
+  let (found, optxpr) =
+    match condopc with
+    | CBranchLessEqual _ ->
+       (match testopc with
+        | CompareImmediate (_, _, _, ra, simm) ->
+           (true, Some (XOp (XLe, [v ra; v simm])))
+        | _ -> (false, None))
+    | _ -> (false, None) in
+
+  if found then
+    match optxpr with
+    | Some expr ->
+       condfloc#set_test_expr expr;
+       testfloc#set_test_variables frozenVars#listOfPairs;
+       (frozenVars#listOfValues, optxpr)
+    | _ -> (frozenVars#listOfValues, None)
+  else
+    (frozenVars#listOfValues, None)
---
+++ CodeHawk/CHB/bchlibpower32/bCHPowerDisassemblyUtils.ml
@@ -0,0 +1,98 @@
+(* =============================================================================
+   CodeHawk Binary Analyzer 
+   Author: Henny Sipma
+   ------------------------------------------------------------------------------
+   The MIT License (MIT)
+ 
+   Copyright (c) 2023  Aarno Labs, LLC
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+ 
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+  
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+   ============================================================================= *)
+
+
+(* Documentation reference:
+ * ========================
+ * ARM Architecture Reference Manual
+ * ARMv7-A and ARMv7-R edition, March 29, 2018
+ *)
+
+(* chlib *)
+open CHLanguage
+open CHPretty
+
+(* chutil *)
+open CHLogger
+
+(* xprlib *)
+open Xprt
+open XprTypes
+
+(* bchlib *)
+open BCHBasicTypes
+open BCHDoubleword
+open BCHLibTypes
+open BCHStrings
+open BCHSystemSettings
+
+(* bchlibelf *)
+open BCHELFHeader
+
+
+module TR = CHTraceResult
+
+
+let get_string_reference (floc:floc_int) (xpr:xpr_t) =
+  try
+    match xpr with
+    | XConst (IntConst num) ->
+       log_tfold_default
+         (mk_tracelog_spec
+            ~tag:"get_string_reference"
+            (floc#cia ^": constant: " ^ num#toString))
+         (fun address ->
+           begin
+	     match elf_header#get_string_at_address address with
+	     | Some str ->
+	        begin
+	          string_table#add_xref address str floc#fa floc#cia;
+                  (if collect_diagnostics () then
+                     ch_diagnostics_log#add
+                       "add string" (LBLOCK [floc#l#toPretty; STR "; "; STR str]));
+	          Some str
+	        end
+	     | _ ->
+                begin
+                  (if collect_diagnostics () then
+                     ch_diagnostics_log#add
+                       "no string found"
+                       (LBLOCK [floc#l#toPretty; STR ": "; address#toPretty]));
+                  None
+                end
+           end)
+         None
+         (numerical_to_doubleword num)
+    | XOp (XPlus, [XVar v; XConst (IntConst num)]) ->
+      if floc#env#has_initialized_string_value v num#toInt then
+	Some (floc#env#get_initialized_string_value v num#toInt)
+      else
+	None
+    | _ -> None
+  with
+  | _ -> None
+              
