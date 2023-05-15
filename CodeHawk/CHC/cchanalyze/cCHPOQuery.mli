200e8389169753e978427823152250614bd11fa2
Henny Sipma <hennysipma@mac.com>
2023-05-15 13:44:04 -0700

    CHC: prepare for OCaml 5

--- CodeHawk/CHC/cchanalyze/cCHPOQuery.mli
+++ CodeHawk/CHC/cchanalyze/cCHPOQuery.mli
@@ -5,6 +5,8 @@
    The MIT License (MIT)
  
    Copyright (c) 2005-2019 Kestrel Technology LLC
+   Copyright (c) 2020-2022 Henny Sipma
+   Copyright (c) 2023      Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -30,6 +32,7 @@ open CCHPreTypes
 
 (* cchanalyze *)
 open CCHAnalysisTypes
+
 
 val mk_poq:
   c_environment_int
