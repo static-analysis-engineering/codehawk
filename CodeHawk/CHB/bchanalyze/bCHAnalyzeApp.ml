56d1605f607770ea353bf3a98bf7f82464111962
Henny Sipma <hennysipma@mac.com>
2023-09-07 22:51:34 -0700

    CHB: add timing and gc options

--- CodeHawk/CHB/bchanalyze/bCHAnalyzeApp.ml
+++ CodeHawk/CHB/bchanalyze/bCHAnalyzeApp.ml
@@ -27,6 +27,8 @@
    SOFTWARE.
    ============================================================================= *)
 
+open Gc
+
 (* chlib *)
 open CHCommon
 open CHPretty
@@ -109,21 +111,29 @@ let fns_excluded = ref []
 let exclude_function s = fns_excluded := s :: !fns_excluded
 
 
-let fns_included = ref []
-let include_function s = fns_included := s :: !fns_included
-
-
-let analyze_x86_function faddr f =
+let analyze_x86_function faddr f count =
+  let _ =
+    if system_settings#show_function_timing then
+      pr_timing [
+          STR "analyzing function (";
+          INT count;
+          STR "): ";
+          faddr#toPretty;
+          STR ": ";
+          INT (List.length f#get_blocks);
+          STR " blocks; ";
+          INT f#get_instruction_count;
+          STR " instructions"] in
+  let _ =
+    if system_settings#gc_compact_function_interval > 0 then
+      (if (count mod system_settings#gc_compact_function_interval) = 0 then
+         begin
+           pr_timing [STR "Initiate Gc.compact"];
+           Gc.compact ();
+           pr_timing [STR "Finish Gc.compact"]
+         end) in
   let fstarttime = Unix.gettimeofday () in
   let finfo =  load_function_info faddr in
-  let _ =
-    pverbose [
-        STR "Analyze ";
-        faddr#toPretty;
-        STR  " (started: ";
-        STR (time_to_string fstarttime);
-        STR ")";
-        NL] in
   let _ = translate_assembly_function f in
   if chif_system#has_procedure_by_address faddr then
     let loopcomplexity = get_cfg_loop_complexity f in
@@ -176,6 +186,7 @@ let analyze_x86_function faddr f =
 
 
 let analyze starttime =
+  let fns_included = included_functions () in
   let count = ref 0 in
   let failedfunctions = ref [] in
   let functionfailure failuretype faddr p =
@@ -192,77 +203,100 @@ let analyze starttime =
     end in
 
   begin
-    assembly_functions#bottom_up_itera
-      (fun faddr f ->
-        if List.mem faddr#to_hex_string !fns_excluded then
-          ()
-        else
-          let callees = get_app_callees faddr in
-          let _ =
-            List.iter (fun callee ->
-	        let cfinfo = get_function_info callee in
-	        if cfinfo#sideeffects_changed then
-	          begin
-	            (get_function_info faddr)#reset_invariants;
-	            chlog#add "reset invariants"
-	              (LBLOCK [
-                           faddr#toPretty;
-                           STR " due to callee ";
-                           callee#toPretty])
-	          end) callees in
-          let appCallees = List.map (fun a -> a#to_hex_string) callees in
-          if file_metrics#is_stable faddr#to_hex_string appCallees &&
-	       (not !analyze_all) then
-            x86_analysis_results#record_results ~save:false f
-          else
-	    let _ = count := !count + 1 in
-            try
-              analyze_x86_function faddr f;
-              pr_interval_timing [STR "functions analyzed: "; INT !count] 60.0
+    (if (List.length fns_included) > 0 then
+       List.iter
+         (fun faddr ->
+           let faddr =
+             fail_tvalue
+               (trerror_record
+                  (LBLOCK [STR "analyze:faddr: "; STR faddr]))
+               (string_to_doubleword faddr) in
+           let f = assembly_functions#get_function_by_address faddr in
+           let _ = count := !count + 1 in
+           try
+             analyze_x86_function faddr f !count;
+             pr_interval_timing [STR "functions analyzed: "; INT !count] 60.0
 	    with
 	    | Failure s -> functionfailure "Failure" faddr (STR s)
 	    | Invalid_argument s -> functionfailure "Invalid argument" faddr (STR s)
 	    | Internal_error s -> functionfailure "Internal error" faddr (STR s)
 	    | Invocation_error s -> functionfailure "Invocation error" faddr (STR s)
 	    | CHFailure p -> functionfailure "CHFailure" faddr p
-	    | BCH_failure p -> functionfailure "BCHFailure" faddr p );
+	    | BCH_failure p -> functionfailure "BCHFailure" faddr p) fns_included
+     else
+       assembly_functions#bottom_up_itera
+         (fun faddr f ->
+           if List.mem faddr#to_hex_string !fns_excluded then
+             ()
+           else
+             let callees = get_app_callees faddr in
+             let _ =
+               List.iter (fun callee ->
+	           let cfinfo = get_function_info callee in
+	           if cfinfo#sideeffects_changed then
+	             begin
+	               (get_function_info faddr)#reset_invariants;
+	               chlog#add "reset invariants"
+	                 (LBLOCK [
+                              faddr#toPretty;
+                              STR " due to callee ";
+                              callee#toPretty])
+	             end) callees in
+             let appCallees = List.map (fun a -> a#to_hex_string) callees in
+             if file_metrics#is_stable faddr#to_hex_string appCallees &&
+	          (not !analyze_all) then
+               x86_analysis_results#record_results ~save:false f
+             else
+	       let _ = count := !count + 1 in
+               try
+                 analyze_x86_function faddr f !count;
+                 pr_interval_timing [STR "functions analyzed: "; INT !count] 60.0
+	       with
+	       | Failure s -> functionfailure "Failure" faddr (STR s)
+	       | Invalid_argument s -> functionfailure "Invalid argument" faddr (STR s)
+	       | Internal_error s -> functionfailure "Internal error" faddr (STR s)
+	       | Invocation_error s -> functionfailure "Invocation error" faddr (STR s)
+	       | CHFailure p -> functionfailure "CHFailure" faddr p
+	       | BCH_failure p -> functionfailure "BCHFailure" faddr p));
 
     let propstarttime = Unix.gettimeofday () in
     let _ = pr_timing [STR "bottom-up analysis finished"] in
-    begin
-      assembly_functions#top_down_itera
-        (fun faddr f ->
-	  try
-	    begin
-	      f#iter_calls (fun _ floc ->
-	          if floc#has_call_target && floc#get_call_target#is_app_call then
-		    let tgtaddr = floc#get_call_target#get_app_address in
-		    let tgtfinfo = get_function_info tgtaddr in
-		    begin
-		      save_function_info tgtfinfo
-		    end );
-	      save_function_summary (get_function_info faddr)
-	    end
-	  with
-	  | Failure s -> functionfailure "Failure" faddr (STR s)
-	  | Invalid_argument s -> functionfailure "Invalid argument" faddr (STR s)
-	  | Internal_error s -> functionfailure "Internal error" faddr (STR s)
-	  | Invocation_error s -> functionfailure "Invocation error" faddr (STR s)
-	  | CHFailure p -> functionfailure "CHFailure" faddr p
-	  | BCH_failure p ->
-	     ch_error_log#add
-               "pushing arguments"
-               (LBLOCK [faddr#toPretty; STR ": "; p]));
-      file_metrics#record_propagation_time ((Unix.gettimeofday ()) -. propstarttime);
-      file_metrics#record_runtime ((Unix.gettimeofday ()) -. starttime);
-      pr_timing [STR "top-down analysis finished"];
-      (match !failedfunctions with
-       | [] -> ()
-       | l ->
-	  chlog#add
-            "failed functions"
-	    (LBLOCK (List.map (fun faddr -> LBLOCK [faddr#toPretty; NL]) l)))
-    end
+    if (List.length fns_included = 0) then
+      begin
+        assembly_functions#top_down_itera
+          (fun faddr f ->
+	    try
+	      begin
+	        f#iter_calls (fun _ floc ->
+	            if floc#has_call_target && floc#get_call_target#is_app_call then
+		      let tgtaddr = floc#get_call_target#get_app_address in
+		      let tgtfinfo = get_function_info tgtaddr in
+                      if not (List.mem tgtaddr !failedfunctions) then
+		        begin
+		          save_function_info tgtfinfo
+		        end);
+	        save_function_summary (get_function_info faddr)
+	      end
+	    with
+	    | Failure s -> functionfailure "Failure" faddr (STR s)
+	    | Invalid_argument s -> functionfailure "Invalid argument" faddr (STR s)
+	    | Internal_error s -> functionfailure "Internal error" faddr (STR s)
+	    | Invocation_error s -> functionfailure "Invocation error" faddr (STR s)
+	    | CHFailure p -> functionfailure "CHFailure" faddr p
+	    | BCH_failure p ->
+	       ch_error_log#add
+                 "pushing arguments"
+                 (LBLOCK [faddr#toPretty; STR ": "; p]));
+        file_metrics#record_propagation_time ((Unix.gettimeofday ()) -. propstarttime);
+        file_metrics#record_runtime ((Unix.gettimeofday ()) -. starttime);
+        pr_timing [STR "top-down analysis finished"];
+        (match !failedfunctions with
+         | [] -> ()
+         | l ->
+	    chlog#add
+              "failed functions"
+	      (LBLOCK (List.map (fun faddr -> LBLOCK [faddr#toPretty; NL]) l)))
+      end
   end
 
 
@@ -337,6 +371,7 @@ let analyze_mips_function faddr f count =
 
 
 let analyze_mips starttime =
+  let fns_included = included_functions () in
   let count = ref 0 in
   let failedfunctions = ref [] in
   let functionfailure failuretype faddr p =
@@ -346,13 +381,13 @@ let analyze_mips starttime =
       failedfunctions := faddr :: !failedfunctions
     end in
   begin
-    (if (List.length !fns_included) > 0 then
+    (if (List.length fns_included) > 0 then
        List.iter
          (fun faddr ->
            let faddr = TR.tget_ok (string_to_doubleword faddr) in
            let f = mips_assembly_functions#get_function_by_address faddr in
            let _ = count := !count + 1 in
-           analyze_mips_function faddr f !count) !fns_included
+           analyze_mips_function faddr f !count) fns_included
      else
        mips_assembly_functions#bottom_up_itera
          (fun faddr f ->
@@ -385,44 +420,53 @@ let analyze_mips starttime =
 
 
 let analyze_arm_function faddr f count =
+  let _ =
+    if system_settings#show_function_timing then
+      pr_timing [
+          STR "analyzing function (";
+          INT count;
+          STR "): ";
+          faddr#toPretty;
+          STR ": ";
+          INT (List.length f#get_blocks);
+          STR " blocks; ";
+          INT f#get_instruction_count;
+          STR " instructions"] in
+  let _ =
+    if system_settings#gc_compact_function_interval > 0 then
+      (if (count mod system_settings#gc_compact_function_interval) = 0 then
+         begin
+           pr_timing [STR "Initiate Gc.compact"];
+           Gc.compact ();
+           pr_timing [STR "Finish Gc.compact"]
+         end) in
   let fstarttime = Unix.gettimeofday () in
   let finfo = load_function_info faddr in
-  let islarge = List.length f#get_blocks > 200 in
-  let _ =
-    pverbose [
-        STR (timing ());
-        STR "translate and analyze ";
-        faddr#toPretty;
-        STR " with ";
-        INT (List.length f#get_blocks);
-        STR " basic blocks ...";
-        NL] in
+  let islarge =
+    ((List.length f#get_blocks)) > 200 || (f#get_instruction_count > 5000) in
   let _ = translate_arm_assembly_function f in
   if arm_chif_system#has_arm_procedure faddr then
-    let _ = pverbose [STR (timing ()); STR "  record loop levels "; NL] in
     let _ = record_arm_loop_levels faddr in
     let proc = arm_chif_system#get_arm_procedure faddr in
     begin
       bb_invariants#reset;
-      pverbose [STR (timing ()); STR "  analyze with intervals ..."; NL];
       analyze_procedure_with_intervals proc arm_chif_system#get_arm_system;
-      pverbose [STR (timing ()); STR "  analyze with linear equalities ..."; NL];
       (if (List.mem faddr#to_hex_string !no_lineq) || islarge then
-         begin
-           chlog#add "skip linear equalities" (faddr#toPretty);
-           pverbose [
-               STR " ** Skip linear equalities ("; faddr#toPretty; STR ")"; NL]
-         end
+         chlog#add
+           "skip linear equalities"
+           (LBLOCK [
+                faddr#toPretty;
+                STR ": ";
+                INT (List.length f#get_blocks);
+                STR " blocks; ";
+                INT f#get_instruction_count;
+                STR " instructions"])
        else
          analyze_procedure_with_linear_equalities
            proc arm_chif_system#get_arm_system);
-      pverbose [STR (timing ()); STR "  analyze with value sets ..."; NL];
       analyze_procedure_with_valuesets proc arm_chif_system#get_arm_system;
-      pverbose [STR (timing ()); STR "  analyze with designations ..."; NL];
       analyze_procedure_with_designations proc arm_chif_system#get_arm_system;
-      pverbose [STR (timing ()); STR "  analyze with reaching defs ..."; NL];
       analyze_procedure_with_reaching_defs proc arm_chif_system#get_arm_system;
-      pverbose [STR (timing ()); STR "  analyze with flag-reaching defs ..."; NL];
       analyze_procedure_with_flag_reaching_defs proc arm_chif_system#get_arm_system;
       (if islarge then
          chlog#add "skip def-use" (faddr#toPretty)
@@ -432,45 +476,50 @@ let analyze_arm_function faddr f count =
          chlog#add "skip def-use-high" (faddr#toPretty)
        else
          analyze_procedure_with_def_use_high proc arm_chif_system#get_arm_system);
-      pverbose [STR (timing ()); STR "  extract ranges ..."; NL];
       extract_ranges finfo bb_invariants#get_invariants;
-      pverbose [STR (timing ()); STR "  exract linear equalities ... "; NL];
       extract_linear_equalities finfo bb_invariants#get_invariants;
-      pverbose [STR (timing ()); STR "  extract value sets ..."; NL];
       extract_valuesets finfo bb_invariants#get_invariants;
-      pverbose [STR (timing ()); STR "  extract designations ..."; NL];
       extract_designations finfo bb_invariants#get_invariants;
-      pverbose [STR (timing ()); STR "  extract reaching defs ..."; NL];
       extract_reaching_defs finfo bb_invariants#get_invariants;
-      pverbose [STR (timing ()); STR "  extract flag-reaching defs ..."; NL];
       extract_flag_reaching_defs finfo bb_invariants#get_invariants;
-      pverbose [STR (timing ()); STR "  extract def-use ..."; NL];
       extract_def_use finfo bb_invariants#get_invariants;
-      pverbose [STR (timing ()); STR "  extract def-use-high ..."; NL];
       extract_def_use_high finfo bb_invariants#get_invariants;
       finfo#reset_invariants;
-      pverbose [STR (timing ()); STR "  save function info ..."; NL];
       save_function_info finfo;
       save_function_invariants finfo;
       save_function_var_invariants finfo;
       arm_analysis_results#record_results f;
       save_function_variables finfo;
-      pverbose [STR (timing ()); STR "  file-metrics ..."; NL];
       file_metrics#record_results
         faddr
         ((Unix.gettimeofday ()) -. fstarttime)
         (get_arm_memory_access_metrics f finfo)
-        (get_arm_cfg_metrics f finfo#env);
-      pverbose [STR (timing ()); STR "  ===function "; faddr#toPretty; STR ":done ===="; NL]
+        (get_arm_cfg_metrics f finfo#env)
     end
   else
-    pr_debug [STR "Translation failed for "; faddr#toPretty; NL]
+    ch_error_log#add
+      "translation failure"
+      (LBLOCK [faddr#toPretty])
 
 
 let analyze_arm starttime =
+  let fns_included = included_functions () in
   let count = ref 0 in
+  let failedfunctions = ref [] in
+  let functionfailure (failuretype: string) (faddr: doubleword_int) (p: pretty_t) =
+    begin
+      ch_error_log#add
+        "function failure"
+        (LBLOCK [
+             STR failuretype;
+             STR ". ";
+             faddr#toPretty;
+             STR ": ";
+             p]);
+      failedfunctions := faddr :: !failedfunctions
+    end in
   begin
-    (if (List.length !fns_included) > 0 then
+    (if (List.length fns_included) > 0 then
        List.iter
          (fun faddr ->
            let faddr =
@@ -480,7 +529,17 @@ let analyze_arm starttime =
                (string_to_doubleword faddr) in
            let f =  arm_assembly_functions#get_function_by_address faddr in
            let _ = count := !count + 1 in
-           analyze_arm_function faddr f !count) !fns_included
+           try
+             analyze_arm_function faddr f !count;
+             pr_interval_timing [STR "functions analyzed: "; INT !count] 60.0
+           with
+	    | Failure s -> functionfailure "Failure" faddr (STR s)
+	    | Invalid_argument s -> functionfailure "Invalid argument" faddr (STR s)
+	    | Internal_error s -> functionfailure "Internal error" faddr (STR s)
+	    | Invocation_error s -> functionfailure "Invocation error" faddr (STR s)
+	    | CHFailure p -> functionfailure "CHFailure" faddr p
+	    | BCH_failure p -> functionfailure "BCHFailure" faddr p) fns_included
+
      else
        arm_assembly_functions#bottom_up_itera
          (fun faddr f ->
@@ -492,10 +551,15 @@ let analyze_arm starttime =
            else
              let _ = count := !count + 1 in
              try
-               analyze_arm_function faddr f !count
+               analyze_arm_function faddr f !count;
+               pr_interval_timing [STR "functions analyzed: "; INT !count] 60.0
              with
-             | BCH_failure p ->
-                raise (BCH_failure p)));
+	       | Failure s -> functionfailure "Failure" faddr (STR s)
+	       | Invalid_argument s -> functionfailure "Invalid argument" faddr (STR s)
+	       | Internal_error s -> functionfailure "Internal error" faddr (STR s)
+	       | Invocation_error s -> functionfailure "Invocation error" faddr (STR s)
+	       | CHFailure p -> functionfailure "CHFailure" faddr p
+	       | BCH_failure p -> functionfailure "BCHFailure" faddr p));
     file_metrics#record_runtime ((Unix.gettimeofday ()) -. starttime)
   end
 
@@ -542,9 +606,10 @@ let analyze_pwr_function
 
 
 let analyze_pwr (starttime: float) =
+  let fns_included = included_functions () in
   let count = ref 0 in
   begin
-    (if (List.length !fns_included) > 0 then
+    (if (List.length fns_included) > 0 then
        List.iter
          (fun faddr ->
            let faddr =
@@ -554,7 +619,7 @@ let analyze_pwr (starttime: float) =
                (string_to_doubleword faddr) in
            let f = pwr_assembly_functions#get_function_by_address faddr in
            let _ = count := !count + 1 in
-           analyze_pwr_function faddr f !count) !fns_included
+           analyze_pwr_function faddr f !count) fns_included
      else
        pwr_assembly_functions#bottom_up_itera
          (fun faddr f ->
