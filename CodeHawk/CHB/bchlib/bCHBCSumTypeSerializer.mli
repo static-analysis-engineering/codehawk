f9b0e246b31cf1636e40483805205ed96a4fe063
Henny Sipma <hennysipma@mac.com>
2023-10-22 21:37:15 -0700

    CHB: move bCHBCSumTypeSerializer to bchlib

--- CodeHawk/CHB/bchlib/bCHBCSumTypeSerializer.mli
+++ CodeHawk/CHB/bchlib/bCHBCSumTypeSerializer.mli
@@ -4,7 +4,7 @@
    ------------------------------------------------------------------------------
    The MIT License (MIT)
  
-   Copyright (c) 2021-2022 Aarno Labs LLC
+   Copyright (c) 2021-2023  Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -29,8 +29,8 @@
 (* chutil *)
 open CHSumTypeSerializer
 
-(* bchcil *)
-open BCHCBasicTypes
+(* bchlib *)
+open BCHBCTypes
 
 
 val ikind_mfts: ikind_t mfts_int
