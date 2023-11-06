a3b4865cc3c4edd8f4ee6b0e238a41da53cabeb3
Henny Sipma <hennysipma@mac.com>
2023-11-05 16:19:55 -0800

    CHB: extract ssa invariants

--- CodeHawk/CHB/bchanalyze/bCHAnalysisTypes.mli
+++ CodeHawk/CHB/bchanalyze/bCHAnalysisTypes.mli
@@ -6,7 +6,7 @@
  
    Copyright (c) 2005-2019 Kestrel Technology LLC
    Copyright (c) 2020      Henny Sipma
-   Copyright (c) 2021-2022 Aarno Labs LLC
+   Copyright (c) 2021-2023 Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -48,7 +48,7 @@ class type constraint_set_int =
 object ('a)
   method add: numerical_constraint_t -> unit
   method get_constraints: numerical_constraint_t list
-  method project_out: variable_t  list -> 'a option
+  method project_out: variable_t list -> 'a option
   method has: variable_t -> bool
   method toPretty: pretty_t
 end
--- CodeHawk/CHB/bchanalyze/bCHTypeConstantsDomainNoArrays.mli
+++ CodeHawk/CHB/bchanalyze/bCHTypeConstantsDomainNoArrays.mli
@@ -4,7 +4,7 @@
    ------------------------------------------------------------------------------
    The MIT License (MIT)
  
-   Copyright (c) 2022-2023  Aarno Labs LLC
+   Copyright (c) 2023  Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -33,7 +33,7 @@ open CHLanguage
 open BCHLibTypes
 
 
-val analyze_procedure_with_def_use: procedure_int -> system_int -> unit
+val analyze_procedure_with_btypes: procedure_int -> system_int -> unit
 
-val extract_def_use:
+val extract_btypes:
   function_info_int -> (string, (string, atlas_t) Hashtbl.t) Hashtbl.t -> unit
