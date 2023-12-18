b7fde8c65fdb0ecb78a0b15d2f78f0e953df2dfc
Henny Sipma <hennysipma@mac.com>
2023-12-18 15:09:26 -0800

    CHB:X86:updates for infrastructure changes

--- CodeHawk/CHB/bchlibx86/bCHFileFormatUtil.ml
+++ CodeHawk/CHB/bchlibx86/bCHFileFormatUtil.ml
@@ -1,12 +1,12 @@
 (* =============================================================================
-   CodeHawk Binary Analyzer 
+   CodeHawk Binary Analyzer
    Author: Henny Sipma
    ------------------------------------------------------------------------------
    The MIT License (MIT)
- 
+
    Copyright (c) 2005-2019 Kestrel Technology LLC
    Copyright (c) 2020-2021 Henny Sipma
-   Copyright (c) 2022      Aarno Labs LLC
+   Copyright (c) 2022-2023 Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -14,10 +14,10 @@
    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
    copies of the Software, and to permit persons to whom the Software is
    furnished to do so, subject to the following conditions:
- 
+
    The above copyright notice and this permission notice shall be included in all
    copies or substantial portions of the Software.
-  
+
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -36,12 +36,13 @@ open CHPretty
 (* chutil *)
 open CHLogger
 open CHXmlDocument
-   
+
 (* bchlib *)
 open BCHBasicTypes
 open BCHDoubleword
 open BCHLibTypes
 open BCHSystemInfo
+open BCHSystemSettings
 
 (* chutil *)
 module TR = CHTraceResult
@@ -55,7 +56,7 @@ module PES = BCHPESections
 module ELFH = BCHELFHeader
 
 
-let is_elf () = system_info#is_elf
+let is_elf () = system_settings#is_elf
 
 
 let register_exn_handler (vaddr:doubleword_int) (bytes:string) =
