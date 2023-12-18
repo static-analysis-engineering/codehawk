a262b45bb2ac33815fad55f7b3fc241cf5885465
Henny Sipma <hennysipma@mac.com>
2023-12-18 13:41:15 -0800

    CHB: update global state interface

--- CodeHawk/CHB/bchlib/bCHGlobalState.mli
+++ CodeHawk/CHB/bchlib/bCHGlobalState.mli
@@ -1,12 +1,12 @@
 (* =============================================================================
-   CodeHawk Binary Analyzer 
+   CodeHawk Binary Analyzer
    Author: Henny Sipma
    ------------------------------------------------------------------------------
    The MIT License (MIT)
- 
+
    Copyright (c) 2005-2019 Kestrel Technology LLC
    Copyright (c) 2020-2021 Henny Sipma
-   Copyright (c) 2022      Aarno Labs LLC
+   Copyright (c) 2022-2023 Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -14,10 +14,10 @@
    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
    copies of the Software, and to permit persons to whom the Software is
    furnished to do so, subject to the following conditions:
- 
+
    The above copyright notice and this permission notice shall be included in all
    copies or substantial portions of the Software.
-  
+
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -33,8 +33,9 @@ open CHPretty
 (* bchlib *)
 open BCHLibTypes
 
+
 val g_arithmetic_op_to_string: g_arithmetic_op -> string
 
-val gterm_to_pretty    : gterm_t -> pretty_t 
+val gterm_to_pretty: gterm_t -> pretty_t
 
 val global_system_state: global_system_state_int
