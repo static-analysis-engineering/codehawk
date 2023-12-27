8cb590fd5b808954db4cc8a607824a1038e282cd
Henny Sipma <hennysipma@mac.com>
2023-12-27 15:51:10 -0800

    CHT:CHB: assertions and utility functions for function-interface tests

--- CodeHawk/CHT/CHB_tests/bchlib_tests/tbchlib/tCHBchlibUtils.ml
+++ CodeHawk/CHT/CHB_tests/bchlib_tests/tbchlib/tCHBchlibUtils.ml
@@ -1,11 +1,11 @@
 (* =============================================================================
-   CodeHawk Unit Testing Framework 
+   CodeHawk Unit Testing Framework
    Author: Henny Sipma
    Adapted from: Kaputt (https://kaputt.x9c.fr/index.html)
    ------------------------------------------------------------------------------
    The MIT License (MIT)
- 
-   Copyright (c) 2022      Aarno Labs LLC
+
+   Copyright (c) 2022-2023  Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -13,10 +13,10 @@
    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
    copies of the Software, and to permit persons to whom the Software is
    furnished to do so, subject to the following conditions:
- 
+
    The above copyright notice and this permission notice shall be included in all
    copies or substantial portions of the Software.
-  
+
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,13 +28,26 @@
 
 (* chlib *)
 open CHLanguage
+open CHPretty
+
+(* chutil *)
+open CHXmlDocument
+open CHXmlReader
 
 (* bchlib *)
+open BCHBasicTypes
+open BCHCPURegisters
+open BCHBCTypes
 open BCHLibTypes
+
+(* tbchlib *)
+open TCHBchlibAssertion
 
 
 let extract_chif_cfg_assignments
-      ?(start=0) ?(len=0) (proc: procedure_int): (variable_t * numerical_exp_t) list =
+      ?(start=0)
+      ?(len=0)
+      (proc: procedure_int): (variable_t * numerical_exp_t) list =
   match proc#getBody#getCmdAt 0 with
   | CFG (_, cfg) ->
      let states = cfg#getStates in
@@ -68,3 +81,85 @@ let extract_chif_cfg_assignments
              end
       | _ -> [])
   | _ -> []
+
+
+type input_parameter_location_t = string * btype_t * string
+
+type input_fts_parameter_t =
+  int * string * btype_t * int * (string * btype_t * string) list
+
+
+let convert_parameter_location_input
+      (loc: input_parameter_location_t): x_fts_loc_t =
+  let (kind, lty, ldata) = loc in
+  match kind with
+  | "s" ->
+     { xftsl_kind = kind;
+       xftsl_type = lty;
+       xftsl_offset = ldata;
+       xftsl_reg = "none"}
+  | "r" ->
+     { xftsl_kind = kind;
+       xftsl_type = lty;
+       xftsl_offset = "-1";
+       xftsl_reg = ldata}
+  | _ ->
+     raise
+       (BCH_failure
+          (LBLOCK [
+               STR "convert_fts_parameter: location kind: ";
+               STR kind;
+               STR " not recognized"]))
+
+
+let input_parameter_location_to_parameter_location
+      (arch: string)
+      (p: input_parameter_location_t): parameter_location_t =
+  let (kind, lty, ldata) = p in
+  let pld = {pld_type = lty; pld_size = 4; pld_extract = None} in
+  match kind with
+  | "s" -> StackParameter (int_of_string ldata, pld)
+  | "r" ->
+     (match arch with
+      | "mips" ->
+         RegisterParameter
+           (MIPSRegister (mipsreg_from_string ldata), pld)
+      | _ ->
+         raise
+           (BCH_failure
+              (LBLOCK [STR "Not yet implemented"])))
+  | _ ->
+     raise
+       (BCH_failure
+          (LBLOCK [STR "Not recognized"]))
+
+let convert_fts_parameter_input (p: input_fts_parameter_t): x_fts_param_t =
+ let (index, name, ty, size, pl) = p in
+     { xfts_index = index;
+       xfts_name = name;
+       xfts_type = ty;
+       xfts_size = size;
+       xfts_locations =
+         List.map convert_parameter_location_input pl
+     }
+
+
+let get_so_xml_summary_string_node
+      (fname: string) (xstring: string): xml_element_int =
+  try
+    let doc = readXmlDocumentString xstring in
+    let root = doc#getRoot in
+    if root#hasOneTaggedChild "libfun" then
+      root#getTaggedChild "libfun"
+    else
+      raise
+        (BCH_failure
+           (LBLOCK [
+                STR "Unable to read function summary string for ";
+                STR fname]))
+  with
+  | XmlParseError (line, col, p)
+    | XmlReaderError (line, col, p)
+    | XmlDocumentError (line, col, p) ->
+     let msg = LBLOCK [STR "Error in file: "; STR fname; STR ": "; p] in
+     raise (XmlDocumentError (line, col, msg))
