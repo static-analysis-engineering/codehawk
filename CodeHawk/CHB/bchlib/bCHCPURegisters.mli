89a3f1fd313ab67f3091b125b34628840087f858
Henny Sipma <hennysipma@mac.com>
2024-01-12 23:53:23 -0800

    CHB: remove register prefixes

--- CodeHawk/CHB/bchlib/bCHCPURegisters.mli
+++ CodeHawk/CHB/bchlib/bCHCPURegisters.mli
@@ -6,7 +6,7 @@
 
    Copyright (c) 2005-2019 Kestrel Technology LLC
    Copyright (c) 2020      Henny Sipma
-   Copyright (c) 2021-2023 Aarno Labs LLC
+   Copyright (c) 2021-2024 Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -219,8 +219,6 @@ val pwr_special_registers: pwr_special_reg_t list
 val register_to_string: register_t -> string
 
 val register_from_string: string -> register_t    (* incomplete *)
-
-val register_to_ssa_prefix: register_t -> string
 
 
 (** {2 x86}*)
