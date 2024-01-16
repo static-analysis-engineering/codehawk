1ebd7f266e3a41008e72046efa9e2dc91e425e6f
Henny Sipma <hennysipma@mac.com>
2024-01-16 00:02:08 -0800

    CHT: fix some warnings

--- CodeHawk/CHT/CHB_tests/bchlibx86_tests/tbchlibx86/tCHBchlibx86Utils.mli
+++ CodeHawk/CHT/CHB_tests/bchlibx86_tests/tbchlibx86/tCHBchlibx86Utils.mli
@@ -5,7 +5,7 @@
    ------------------------------------------------------------------------------
    The MIT License (MIT)
  
-   Copyright (c) 2023  Aarno Labs LLC
+   Copyright (c) 2023-2024  Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -25,12 +25,3 @@
    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
    SOFTWARE.
    ============================================================================= *)
-
-(* chlib *)
-open CHLanguage
-
-(* xprlib *)
-open XprTypes
-
-(* bchlib *)
-open BCHLibTypes
