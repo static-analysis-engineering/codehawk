905ac9f9e4344197f1081f1c5a808347033fd921
Henny Sipma <hennysipma@mac.com>
2024-01-16 16:02:13 -0800

    CHJ:jchlib: fix warnings and reformat

--- CodeHawk/CHJ/jchlib/jCHIFUtil.mli
+++ CodeHawk/CHJ/jchlib/jCHIFUtil.mli
@@ -1,10 +1,11 @@
 (* =============================================================================
-   CodeHawk Java Analyzer 
+   CodeHawk Java Analyzer
    Author: Henny Sipma
    ------------------------------------------------------------------------------
    The MIT License (MIT)
- 
+
    Copyright (c) 2005-2020 Kestrel Technology LLC
+   Copyright (c) 2020-2024 Henny B. Sipma
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -12,10 +13,10 @@
    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
    copies of the Software, and to permit persons to whom the Software is
    furnished to do so, subject to the following conditions:
- 
+
    The above copyright notice and this permission notice shall be included in all
    copies or substantial portions of the Software.
-  
+
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,14 +25,21 @@
    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
    SOFTWARE.
    ============================================================================= *)
+(** Utility functions for CHIF data types *)
 
 (* chlib *)
 open CHLanguage
 
+
+(** [add_attribute sym att] adds [att] to the attribute list of [sym] *)
 val add_attribute: symbol_t -> string -> symbol_t
 
+(** [get_input_vars b sig] returns the READ/READ_WRITE variables from the
+    bindings [b] with signature [sig] *)
 val get_input_vars: bindings_t -> signature_t -> variable_t list
+
+(** [get_output_vars b sig] returns the WRITE/READ_WRITE variables from the
+    bindings [b] with signature [sig] *)
 val get_output_vars: bindings_t -> signature_t -> variable_t list
 
 val is_last_opcode_for_line: int -> (int * int list) list -> bool
-
