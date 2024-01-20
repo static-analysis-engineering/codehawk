f0f4fe0ab57913909740eee3c03d970e45ae908c
Henny Sipma <hennysipma@mac.com>
2024-01-19 16:17:11 -0800

    CHJ:jchlib: fix warnings and reformat

--- CodeHawk/CHJ/jchlib/jCHFile.mli
+++ CodeHawk/CHJ/jchlib/jCHFile.mli
@@ -1,10 +1,11 @@
 (* =============================================================================
-   CodeHawk Java Analyzer 
+   CodeHawk Java Analyzer
    Author: Arnaud Venet
    ------------------------------------------------------------------------------
    The MIT License (MIT)
- 
+
    Copyright (c) 2005-2020 Kestrel Technology LLC
+   Copyright (c) 2020-2024 Henny B. Sipma
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -12,10 +13,10 @@
    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
    copies of the Software, and to permit persons to whom the Software is
    furnished to do so, subject to the following conditions:
- 
+
    The above copyright notice and this permission notice shall be included in all
    copies or substantial portions of the Software.
-  
+
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -32,24 +33,29 @@ open CHPretty
 open JCHBasicTypesAPI
 open JCHRawClass
 
+
 exception No_class_found of string
 
-type cp_unit_t = 
-| Directory of string 
-| JarFile of string * Zip.in_file 
+type cp_unit_t =
+| Directory of string
+| JarFile of string * Zip.in_file
 | WarFile of string * Zip.in_file
 
 type class_path_t = cp_unit_t list
 
 val class_path : string -> cp_unit_t list
+
 val close_class_path: cp_unit_t list -> unit
 
 val get_classpath_units : string -> string list
 
 val class_path_to_pretty: class_path_t -> pretty_t
 
-val get_class : class_path_t -> < name : string; .. > -> java_class_or_interface_int
+val get_class :
+  class_path_t -> < name : string; .. > -> java_class_or_interface_int
+
 val has_summary_class: class_path_t -> < name:string ; .. > -> bool
+
 val get_summary_class: class_path_t -> < name:string ; .. > -> string
 
 val get_summary_manifest: cp_unit_t -> string
@@ -60,4 +66,7 @@ val apply_to_jar :
   (Zip.in_file -> Zip.entry -> unit) -> string -> unit
 
 val apply_to_xml_jar :
-  (string -> string -> unit) -> (Zip.in_file -> Zip.entry -> unit) -> string -> unit
+  (string -> string -> unit)
+  -> (Zip.in_file -> Zip.entry -> unit)
+  -> string
+  -> unit
--- CodeHawk/CHJ/jchlib/jCHTranslateToCHIF.mli
+++ CodeHawk/CHJ/jchlib/jCHTranslateToCHIF.mli
@@ -1,11 +1,11 @@
 (* =============================================================================
-   CodeHawk Java Analyzer 
+   CodeHawk Java Analyzer
    Author: Arnaud Venet
    ------------------------------------------------------------------------------
    The MIT License (MIT)
- 
+
    Copyright (c) 2005-2020 Kestrel Technology LLC
-   Copyright (c) 2020-2023 Henny Sipma
+   Copyright (c) 2020-2024 Henny Sipma
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -13,10 +13,10 @@
    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
    copies of the Software, and to permit persons to whom the Software is
    furnished to do so, subject to the following conditions:
- 
+
    The above copyright notice and this permission notice shall be included in all
    copies or substantial portions of the Software.
-  
+
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,8 +26,6 @@
    SOFTWARE.
    ============================================================================= *)
 
-open Big_int_Z
-
 (* chlib *)
 open CHPretty
 open CHLanguage
@@ -35,11 +33,13 @@ open CHLanguage
 (* jchlib *)
 open JCHBasicTypesAPI
 
+
 val handler_pc_label: int -> string
 
 val get_register_variable: int -> value_type_t -> variable_t
 
-val set_command_pretty_printer: (int -> ((code_int,cfg_int) command_t -> pretty_t)) -> unit
+val set_command_pretty_printer:
+  (int -> ((code_int,cfg_int) command_t -> pretty_t)) -> unit
 
 val translation_failed : CHUtils.SymbolCollections.set_t
 
@@ -51,16 +51,18 @@ val make_exn_info:
   -> (class_name_int -> catch_info_t) -> exn_info_int
 
 val defaultExnInfo: exn_info_int
+
 val defaultExnInfoFn: method_int -> (int -> exn_info_int list)
 
 val make_stack_variable: int -> int -> variable_type_t -> variable_t
+
 val make_register_variable: int -> variable_type_t -> variable_t
 
 val translate_method :
   proc_filter:(procedure_int -> bool) ->
   simplify:bool ->
   transform:bool ->
-  java_method:method_int -> code_set:system_int -> 
+  java_method:method_int -> code_set:system_int ->
   ?exn_infos_fn:(int -> exn_info_int list) ->
   ?initialization_cmd:(code_int, cfg_int) command_t -> unit -> unit
 
@@ -75,4 +77,3 @@ val make_code_set : symbol_t -> system_int
 val get_procname_symbol: class_method_signature_int -> symbol_t
 
 val get_method_stack_layout: system_int -> method_int -> method_stack_layout_int
-
