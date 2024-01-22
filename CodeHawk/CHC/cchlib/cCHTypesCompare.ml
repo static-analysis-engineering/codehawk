13f44d528ea83d832f0f32dae17a8dc7e425b164
Henny Sipma <hennysipma@mac.com>
2024-01-22 00:29:54 -0800

    CHC:cchlib fix warnings and reformat

--- CodeHawk/CHC/cchlib/cCHTypesCompare.ml
+++ CodeHawk/CHC/cchlib/cCHTypesCompare.ml
@@ -1,12 +1,12 @@
 (* =============================================================================
-   CodeHawk C Analyzer 
+   CodeHawk C Analyzer
    Author: Henny Sipma
    ------------------------------------------------------------------------------
    The MIT License (MIT)
- 
+
    Copyright (c) 2005-2019 Kestrel Technology LLC
    Copyright (c) 2020      Henny Sipma
-   Copyright (c) 2021      Aarno Labs LLC
+   Copyright (c) 2021-2024 Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -14,10 +14,10 @@
    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
    copies of the Software, and to permit persons to whom the Software is
    furnished to do so, subject to the following conditions:
- 
+
    The above copyright notice and this permission notice shall be included in all
    copies or substantial portions of the Software.
-  
+
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,13 +27,11 @@
    SOFTWARE.
    ============================================================================= *)
 
-(* extchlib *)
-open CHPretty
-
 (* cchlib *)
 open CCHBasicTypes
 
 module H = Hashtbl
+
 
 let list_compare (l1:'a list) (l2:'b list) (f:'a -> 'b -> int):int =
   let length = List.length in
@@ -44,14 +42,6 @@ let list_compare (l1:'a list) (l2:'b list) (f:'a -> 'b -> int):int =
   else
     List.fold_right2 (fun e1 e2 a -> if a = 0 then (f e1 e2) else a) l1 l2 0
 
-let list_table_compare (l1:'a list) (l2:'b list) (f:'a -> 'b -> int):int =
-  let length = List.length in
-  if (length l1) < (length l2) then
-    -1
-  else if (length l1) > (length l2) then
-    1
-  else
-    List.fold_right2 (fun e1 e2 a -> if a = 0 then (f e1 e2) else a) l1 l2 0
 
 let option_compare  (x1:'a option) (x2:'a option) (f:'a -> 'a -> int): int =
   match (x1,x2) with
@@ -59,7 +49,8 @@ let option_compare  (x1:'a option) (x2:'a option) (f:'a -> 'a -> int): int =
   | (Some _, _) -> -1
   | (_, Some _) -> 1
   | _ -> 0
-    
+
+
 let pair_compare
       (p1:('a * 'b))
       (p2:('a * 'b))
@@ -67,6 +58,7 @@ let pair_compare
       (f2:'b -> 'b -> int):int =
   let l0 = f1 (fst p1) (fst p2) in
   if l0 = 0 then f2 (snd p1) (snd p2) else l0
+
 
 let triple_compare
       (p1:('a * 'b * 'c))
@@ -84,16 +76,11 @@ let triple_compare
     else l1
   else l0
 
+
 let varuse_compare (v1: varuse) (v2: varuse) = Stdlib.compare v1 v2
 
 let fielduse_compare (f1: fielduse) (f2: fielduse) = Stdlib.compare f1 f2
 
-let table_to_pretty t =
-  let p = ref [] in
-  let _ =
-    H.iter (fun k v ->
-        p := (LBLOCK [INT k; STR " -> "; INT v; NL]) :: !p) t in
-  LBLOCK [STR "table: "; NL; INDENT (3,LBLOCK !p); NL]
 
 let location_compare (loc1:location) (loc2:location) =
   let l0 = Stdlib.compare loc1.byte loc2.byte in
@@ -106,18 +93,19 @@ let location_compare (loc1:location) (loc2:location) =
   else
     l0
 
-let rec typ_compare 
+
+let rec typ_compare
     ?(ctable:(int,int) H.t option)
     ?(constr:(int,int) H.t option)
     (t1:typ) (t2:typ) =
-  let funarg_list_compare (a1:funarg list) (a2:funarg list) = 
+  let funarg_list_compare (a1:funarg list) (a2:funarg list) =
     let length = List.length in
     if (length a1) < (length a2) then
       -1
     else if (length a1) > (length a2) then
       1
-    else 
-      List.fold_right2 
+    else
+      List.fold_right2
 	(fun (_,at1,_) (_,at2,_) a ->
           if a = 0 then
 	    typ_compare ?ctable ?constr at1 at2
@@ -137,15 +125,15 @@ let rec typ_compare
   | (TPtr _, _) -> -1
   | (_, TPtr _) -> 1
   | (TArray (t1,e1,_), TArray (t2,e2,_)) ->
-    let l0 = typ_compare ?ctable ?constr t1 t2 in 
+    let l0 = typ_compare ?ctable ?constr t1 t2 in
     if l0 = 0 then option_compare e1 e2 exp_compare else l0
   | (TArray _, _) -> -1
   | (_, TArray _) -> 1
   | (TFun (rt1,a1,v1,_), TFun (rt2,a2,v2,_)) ->
     let l0 = typ_compare ?ctable ?constr rt1 rt2 in
     if l0 = 0 then
-      let l1 = match (a1,a2) with 
-	| (Some al1,Some al2) -> funarg_list_compare al1 al2 
+      let l1 = match (a1,a2) with
+	| (Some al1,Some al2) -> funarg_list_compare al1 al2
 	| (Some _, _) -> -1
 	| (_, Some _) -> 1
 	| (None,None) -> 0 in
@@ -171,7 +159,8 @@ let rec typ_compare
   | (TEnum _, _) -> -1
   | (_, TEnum _) -> 1
   | (TBuiltin_va_list _, TBuiltin_va_list _) -> 0
-    
+
+
 and constant_compare (c1:constant) (c2:constant) =
   match (c1,c2) with
   | (CInt (i1,_,_), CInt(i2,_,_)) -> Int64.compare i1 i2
@@ -191,13 +180,14 @@ and constant_compare (c1:constant) (c2:constant) =
   | (_, CReal _) -> 1
   | (CEnum (e1,iname1,ename1), CEnum (e2,iname2,ename2)) ->
     let l0 = Stdlib.compare iname1 iname2 in
-    if l0 = 0 then 
+    if l0 = 0 then
       let l1 = Stdlib.compare ename1 ename2 in
       if l1 = 0 then
 	exp_compare e1 e2
       else l1
     else l0
-      
+
+
 and offset_compare (o1:offset) (o2:offset) =
   match (o1,o2) with
   | (NoOffset, NoOffset) -> 0
@@ -207,18 +197,23 @@ and offset_compare (o1:offset) (o2:offset) =
     let l0 = fielduse_compare f1 f2 in if l0 = 0 then offset_compare o1 o2 else l0
   | (Field _, _) -> -1
   | (_, Field _) -> 1
-	| (Index (e1,o1), Index (e2,o2)) ->
-	  let l0 = exp_compare e1 e2 in if l0 = 0 then offset_compare o1 o2 else l0
-	      
+  | (Index (e1,o1), Index (e2,o2)) ->
+     let l0 = exp_compare e1 e2 in
+     if l0 = 0 then offset_compare o1 o2 else l0
+
+
 and lhost_compare (h1:lhost) (h2:lhost) =
   match (h1,h2) with
   | (Var v1, Var v2) -> varuse_compare v1 v2
   | (Var _, _) -> -1
   | (_, Var _) -> 1
   | (Mem e1, Mem e2) -> exp_compare e1 e2
-    
+
+
 and lval_compare (v1:lval) (v2:lval) =
-  let l0 = lhost_compare (fst v1) (fst v2) in if l0 = 0 then offset_compare (snd v1) (snd v2) else l0
+  let l0 = lhost_compare (fst v1) (fst v2) in
+  if l0 = 0 then offset_compare (snd v1) (snd v2) else l0
+
 
 and opt_exp_compare e1 e2 =
   match (e1,e2) with
@@ -226,7 +221,8 @@ and opt_exp_compare e1 e2 =
   | (Some _,None) -> -1
   | (None,Some _) -> 1
   | (None,None) -> 0
-    
+
+
 and exp_compare (e1:exp) (e2:exp) =
   match (e1,e2) with
   | (Const c1, Const c2) -> constant_compare c1 c2
@@ -252,7 +248,7 @@ and exp_compare (e1:exp) (e2:exp) =
   | (_, AlignOfE _) -> 1
   | (UnOp (op1,e1,t1), UnOp (op2,e2,t2)) ->
     let l0 = Stdlib.compare op1 op2 in
-    if l0 = 0 then 
+    if l0 = 0 then
       let l1 = exp_compare e1 e2 in
       if l1 = 0 then typ_compare t1 t2
       else l1
@@ -263,7 +259,7 @@ and exp_compare (e1:exp) (e2:exp) =
     let l0 = Stdlib.compare op1 op2 in
     if l0 = 0 then
       let l1 = exp_compare x1 x2 in
-      if l1 = 0 then 
+      if l1 = 0 then
 	let l2 = exp_compare y1 y2 in
 	if l2 = 0 then
 	  typ_compare t1 t2
@@ -285,7 +281,7 @@ and exp_compare (e1:exp) (e2:exp) =
     else l0
   | (Question _, _) -> -1
   | (_, Question _) -> 1
-  | (CastE (t1,e1), CastE (t2,e2)) ->	
+  | (CastE (t1,e1), CastE (t2,e2)) ->
     let l0 = typ_compare t1 t2 in if l0 = 0 then exp_compare e1 e2 else l0
   | (CastE _, _) -> -1
   | (_, CastE _) -> 1
@@ -306,13 +302,14 @@ and exp_compare (e1:exp) (e2:exp) =
     else l0
   | (FnApp _,_) -> -1
   | (_,FnApp _) -> 1
-  | (CnApp (s1,el1,t1),CnApp (s2,el2,t2)) ->
+  | (CnApp (s1, el1, _t1),CnApp (s2, el2, _t2)) ->
     let l0 = Stdlib.compare s1 s2 in
     if l0 = 0 then
       list_compare el1 el2 opt_exp_compare
     else
       l0
-      
+
+
 and eitem_compare (eitem1:eitem) (eitem2:eitem) =
   let (iname1,iexp1,_) = eitem1 in
   let (iname2,iexp2,_) = eitem2 in
@@ -321,44 +318,51 @@ and eitem_compare (eitem1:eitem) (eitem2:eitem) =
     exp_compare iexp1 iexp2
   else
     l0
-      
-and fieldinfo_structural_compare 
-    (ctable:(int,int) H.t)
-    (constr:(int,int) H.t)
+
+
+and fieldinfo_structural_compare
+    (ctable:(int, int) H.t)
+    (constr:(int, int) H.t)
     (finfo1:fieldinfo) (finfo2:fieldinfo) =
   let l0 = Stdlib.compare finfo1.fname finfo2.fname in
   if l0 = 0 then
     typ_compare ~ctable ~constr finfo1.ftype finfo2.ftype
   else
     l0
-    
-and enuminfo_structural_compare einfo1 einfo2 : int = 
+
+
+and enuminfo_structural_compare einfo1 einfo2 : int =
   list_compare einfo1.eitems einfo2.eitems eitem_compare
-    
+
+
 and compinfo_structural_compare
     (ctable:(int,int) H.t)
     (cinfo1:compinfo) (cinfo2:compinfo) =
   let constr = H.create 3 in
   let l0 = Stdlib.compare cinfo1.cstruct cinfo2.cstruct in
   let result =
-    if l0 = 0 then	
-      list_compare cinfo1.cfields cinfo2.cfields 
+    if l0 = 0 then
+      list_compare cinfo1.cfields cinfo2.cfields
 	(fieldinfo_structural_compare ctable constr) else l0 in
   (result, constr)
 
-and fieldinfo_compare (finfo1:fieldinfo) (finfo2:fieldinfo) =
+
+and _fieldinfo_compare (finfo1:fieldinfo) (finfo2:fieldinfo) =
   let l0 = Stdlib.compare finfo1.fname finfo2.fname in
   if l0 = 0 then typ_compare finfo1.ftype finfo2.ftype else l0
-    
+
+
 and compinfo_compare (cinfo1:compinfo) (cinfo2:compinfo) =
   let l0 = Stdlib.compare cinfo1.cstruct cinfo2.cstruct in
   if l0 = 0 then
     let h1 = H.create 3 in
     let h2 = H.create 3 in
-    list_compare cinfo1.cfields cinfo2.cfields (fieldinfo_structural_compare h1 h2)
+    list_compare
+      cinfo1.cfields cinfo2.cfields (fieldinfo_structural_compare h1 h2)
   else
     l0
-      
+
+
 let varinfo_compare (vinfo1:varinfo) (vinfo2:varinfo) =
   match (vinfo1.vstorage,vinfo2.vstorage) with
   | (Static,Static) ->
@@ -367,4 +371,3 @@ let varinfo_compare (vinfo1:varinfo) (vinfo2:varinfo) =
   | (Static,_) -> -1
   | (_, Static) -> 1
   | _ -> Stdlib.compare vinfo1.vname vinfo2.vname
-    
--- CodeHawk/CHC/cchlib/cCHTypesTransformer.mli
+++ CodeHawk/CHC/cchlib/cCHTypesTransformer.mli
@@ -1,10 +1,12 @@
 (* =============================================================================
-   CodeHawk C Analyzer 
+   CodeHawk C Analyzer
    Author: Henny Sipma
    ------------------------------------------------------------------------------
    The MIT License (MIT)
- 
+
    Copyright (c) 2005-2019 Kestrel Technology LLC
+   Copyright (c) 2020-2023 Henny B. Sipma
+   Copyright (c) 2024      Aarno Labs LLC
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -12,10 +14,10 @@
    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
    copies of the Software, and to permit persons to whom the Software is
    furnished to do so, subject to the following conditions:
- 
+
    The above copyright notice and this permission notice shall be included in all
    copies or substantial portions of the Software.
-  
+
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,39 +29,42 @@
 
 (* cchlib *)
 open CCHBasicTypes
-open CCHLibTypes
+
 
 class exp_transformer_t:
-object 
-  method transform_exp   : exp -> exp
-  method transform_type  : typ -> typ
-  method transform_lval  : lval -> lval
-  method transform_lhost : lhost -> lhost
+object
+  method transform_exp: exp -> exp
+  method transform_type: typ -> typ
+  method transform_lval: lval -> lval
+  method transform_lhost: lhost -> lhost
   method transform_offset: offset -> offset
 end
 
+
 class exp_walker_t:
 object
-  method walk_exp   : exp -> unit
-  method walk_type  : typ -> unit
-  method walk_lval  : lval -> unit
-  method walk_lhost : lhost -> unit
+  method walk_exp: exp -> unit
+  method walk_type: typ -> unit
+  method walk_lval: lval -> unit
+  method walk_lhost: lhost -> unit
   method walk_offset: offset -> unit
 end
 
+
 class block_walker_t:
 object
-  method walk_block : block -> unit
-  method walk_stmt  : stmt -> unit
+  method walk_block: block -> unit
+  method walk_stmt: stmt -> unit
   method walk_stmtkind: stmtkind -> unit
-  method walk_label : label -> unit
-  method walk_instr : instr -> unit
-  method walk_rhs   : exp -> unit          (* evaluated only *)
-  method walk_lhs   : lval -> unit         (* lhs in assignment *)
-  method walk_arg   : exp -> unit          (* argument passed to function *)
-  method walk_calltarget: exp -> unit    
+  method walk_label: label -> unit
+  method walk_instr: instr -> unit
+  method walk_rhs: exp -> unit          (* evaluated only *)
+  method walk_lhs: lval -> unit         (* lhs in assignment *)
+  method walk_arg: exp -> unit          (* argument passed to function *)
+  method walk_calltarget: exp -> unit
 end
 
 
 val get_exp_variables: exp -> int list         (* vid's *)
+
 val get_block_variables: block -> int list     (* vid's *)
