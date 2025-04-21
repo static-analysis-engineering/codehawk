9b54f55be264e94bb6d3568ec8f6894e916631b6
Henny Sipma <hennysipma@mac.com>
2025-04-20 17:51:34 -0700

    CHB:ARM: add global variable addresses to assembly listing

--- CodeHawk/CHB/bchlib/bCHVersion.ml
+++ CodeHawk/CHB/bchlib/bCHVersion.ml
@@ -95,8 +95,8 @@ end
 
 
 let version = new version_info_t 
-  ~version:"0.6.0_20250417"
-  ~date:"2025-04-17"
+  ~version:"0.6.0_20250420"
+  ~date:"2025-04-20"
   ~licensee: None
   ~maxfilesize: None
   ()
