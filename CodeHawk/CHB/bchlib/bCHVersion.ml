c05ec386ba972a68beffdd1a147a03bc3be1ba7e
Henny Sipma <hennysipma@mac.com>
2025-07-16 19:59:32 -0700

    CHB: handle base-ptr array offset

--- CodeHawk/CHB/bchlib/bCHVersion.ml
+++ CodeHawk/CHB/bchlib/bCHVersion.ml
@@ -95,8 +95,8 @@ end
 
 
 let version = new version_info_t 
-  ~version:"0.6.0_20250714"
-  ~date:"2025-07-14"
+  ~version:"0.6.0_20250716"
+  ~date:"2025-07-16"
   ~licensee: None
   ~maxfilesize: None
   ()
