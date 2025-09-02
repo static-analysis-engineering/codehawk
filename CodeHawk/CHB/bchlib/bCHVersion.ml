083f306dd8be2d7813e02d59a0e5af3a62110558
Henny Sipma <hennysipma@mac.com>
2025-09-02 16:18:40 -0700

    CHB:ARM: allow for printing data sections

--- CodeHawk/CHB/bchlib/bCHVersion.ml
+++ CodeHawk/CHB/bchlib/bCHVersion.ml
@@ -95,8 +95,8 @@ end
 
 
 let version = new version_info_t 
-  ~version:"0.6.0_20250830"
-  ~date:"2025-08-30"
+  ~version:"0.6.0_20250902"
+  ~date:"2025-09-02"
   ~licensee: None
   ~maxfilesize: None
   ()
